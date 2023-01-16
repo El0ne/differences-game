41b7bd08bc59c99bb8f512f8a93d640d55bfe8a3
Chahine <tchamitouche@gmail.com>
2023-01-16 15:18:35 -0500

    deleting more problematic lines

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
-import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { GameCardSelectionComponent } from './components/game-card-selection/game-card-selection.component';
@@ -18,7 +17,7 @@ import { GameCardSelectionComponent } from './components/game-card-selection/gam
  * Otherwise Angular Cli will not know in which module to put new component
  */
 @NgModule({
-    declarations: [AppComponent, GamePageComponent, MainPageComponent, MaterialPageComponent, GameCardSelectionComponent],
+    declarations: [AppComponent, MainPageComponent, MaterialPageComponent, GameCardSelectionComponent],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
