08500b5f2ae204438df3f6dfe43f46ee7cad1627
Chahine <tchamitouche@gmail.com>
2023-01-16 15:07:55 -0500

    adding game card component

---
+++ client/src/app/components/game-card-selection/game-card-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-card-selection',
+  templateUrl: './game-card-selection.component.html',
+  styleUrls: ['./game-card-selection.component.scss']
+})
+export class GameCardSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
