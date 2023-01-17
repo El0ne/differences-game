b8b5200522a38f87fd173f334220b0c806e92761
Alexis Nicolas <alexisducanada@hotmail.com>
2023-01-17 14:26:01 -0500

    tried testing game-selection componenent, will wait until the structure is clearer

--- client/src/app/pages/game-selection/game-selection.component.spec.ts
+++ client/src/app/pages/game-selection/game-selection.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+//import { By } from '@angular/platform-browser';
+import { GameInformation } from '@app/classes/game-information';
 
 import { GameSelectionComponent } from './game-selection.component';
 
@@ -21,10 +23,31 @@ describe('GameSelectionComponent', () => {
     });
 
     it('currentGameCards should be equal to gameCards lenght until it is bigger than 4', () => {
-        expect(component.currentGameCards.length).toBe(0);
+        for (let i: number = 0; i < 4; i++) {
+            expect(component.currentGameCards.length).toBe(i);
+            component.gameCards.push(new GameInformation());
+            component.selectGameCards();
+        }
     });
 
-    it('first game cards to show should be the 4 first ones', () => {
-        expect(component.currentGameCards.length).toBe(0);
+    it('selectGameCards should put 0 gameCards in currentGameCards if gameCards is empty', () => {
+        component.selectGameCards();
+        expect(component.currentGameCards.length).toBe(0);  
+    });
+
+    it('next-button should show the next 4 elements until there is fewer than 4 to show', () => {
+        for (let i: number = 0; i < 10; i++) {
+            component.gameCards.push(new GameInformation());
+        }
+        let button: HTMLElement | null = document.getElementById("next-button");
+        //expect(component.currentGameCards.length).toBe(4);//does not work because the first selectGameCards() has been called before pushing gameCards
+        //expect(fixture.debugElement.queryAll(By.css('.game-card-container > div')).length).toBe(4);//selector does not work
+        button?.click();
+        expect(component.currentGameCards.length).toBe(4);
+        //expect(fixture.debugElement.queryAll(By.css('.game-card-container > div')).length).toBe(4);
+        button?.click();
+        expect(component.currentGameCards.length).toBe(2);
+        //expect(fixture.debugElement.queryAll(By.css('.game-card-container > div')).length).toBe(2);
     });
 });
+    
