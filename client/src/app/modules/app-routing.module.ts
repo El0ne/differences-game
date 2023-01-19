d639a0bab0aa151ddb95fb934995aa9e54966802
JulienLegare <julien.legare@polymtl.ca>
2023-01-18 19:02:02 -0500

    commit of the deleteed files

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GamePageComponent } from '@app/pages/game-page/game-page.component';
-import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { HomePageComponent } from '@app/pages/home-page/home-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: MainPageComponent },
-    { path: 'game', component: GamePageComponent },
-    { path: 'material', component: MaterialPageComponent },
+    { path: 'home', component: HomePageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
