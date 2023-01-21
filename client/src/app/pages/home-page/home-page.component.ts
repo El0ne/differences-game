5cdbb38327f8f2bd9dc4294d403550952c311b33
JulienLegare <julien.legare@polymtl.ca>
2023-01-21 11:58:24 -0500

    readded more files

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,16 @@
+<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+=======
+import { Component, OnInit } from '@angular/core';
+>>>>>>> parent of f037a05 (deleted all useless files)
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
 })
+<<<<<<< HEAD
 export class HomePageComponent {
     constructor(private router: Router) {}
     // TODO : IMPLEMENT FUNCTIONS TO MATCH WANTED BEHAVIOUR
@@ -17,5 +22,21 @@ export class HomePageComponent {
     }
     showConfig() {
         this.router.navigate(['/home']); // TODO : REPLACE PLACEHOLDER BY ACTUAL PATH
+=======
+export class HomePageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    // TODO : IMPLEMENT FUNCTIONS TO MATCH WANTED BEHAVIOUR
+    showClassic() {
+        console.log('click working 1');
+    }
+    showLimitedTime() {
+        console.log('click working 2');
+    }
+    showConfig() {
+        console.log('click working 3');
+>>>>>>> parent of f037a05 (deleted all useless files)
     }
 }
