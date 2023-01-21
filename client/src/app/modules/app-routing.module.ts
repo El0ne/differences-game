1f68b532f19acf107d11f670ca7ab5398e195726
JulienLegare <julien.legare@polymtl.ca>
2023-01-21 11:57:45 -0500

    readded files that shouldnt have been deleted

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from '@app/pages/home-page/home-page.component';
+import { GamePageComponent } from '@app/pages/game-page/game-page.component';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomePageComponent },
+    { path: 'home', component: MainPageComponent },
+    { path: 'game', component: GamePageComponent },
+    { path: 'material', component: MaterialPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
