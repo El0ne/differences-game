2c357f477158fa6dab9732924d6fe81dde08997e
JulienLegare <julien.legare@polymtl.ca>
2023-01-23 10:47:54 -0500

    changes to the buttons, use routerLink now

--- client/src/app/pages/home-page/home-page.component.spec.ts
+++ client/src/app/pages/home-page/home-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomePageComponent } from './home-page.component';
 
@@ -20,38 +20,4 @@ describe('HomePageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-
-    /*
-    it('should call showClassic on click', () => {
-        const spy = spyOn(component, 'showClassic');
-        const button = fixture.debugElement.nativeElement.querySelector(By.css('classic'));
-
-        button.click();
-        expect(spy).toHaveBeenCalled();
-    });
-    */
-
-    it('should call showClassic on click', fakeAsync(() => {
-        spyOn(component, 'showClassic');
-
-        const button = fixture.debugElement.nativeElement.querySelector("[id='classic']");
-        button.click();
-        expect(component.showClassic).toHaveBeenCalled();
-    }));
-
-    it('should call showLimitedTime on click', fakeAsync(() => {
-        spyOn(component, 'showLimitedTime');
-
-        const button = fixture.debugElement.nativeElement.querySelector("[id='limitedTime']");
-        button.click();
-        expect(component.showLimitedTime).toHaveBeenCalled();
-    }));
-
-    it('should call showConfig on click', fakeAsync(() => {
-        spyOn(component, 'showConfig');
-
-        const button = fixture.debugElement.nativeElement.querySelector("[id='config']");
-        button.click();
-        expect(component.showConfig).toHaveBeenCalled();
-    }));
 });
--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,21 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-    constructor(private router: Router) {}
-    // TODO : IMPLEMENT FUNCTIONS TO MATCH WANTED BEHAVIOUR
-    showClassic() {
-        this.router.navigate(['/home']); // TODO : REPLACE PLACEHOLDER BY ACTUAL PATH
-    }
-    showLimitedTime() {
-        this.router.navigate(['/home']); // TODO : REPLACE PLACEHOLDER BY ACTUAL PATH
-    }
-    showConfig() {
-        this.router.navigate(['/home']); // TODO : REPLACE PLACEHOLDER BY ACTUAL PATH
-    }
-}
+export class HomePageComponent {}
