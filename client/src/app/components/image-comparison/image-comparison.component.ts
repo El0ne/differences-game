dd8bfe6b4bb2b85eecfd2ce321c56afbf58bf799
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-01-24 09:17:39 -0500

    Started canvas. Can fill a rectangle with color

--- client/src/app/components/image-comparison/image-comparison.component.ts
+++ client/src/app/components/image-comparison/image-comparison.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-image-comparison',
-  templateUrl: './image-comparison.component.html',
-  styleUrls: ['./image-comparison.component.scss']
+    selector: 'app-image-comparison',
+    templateUrl: './image-comparison.component.html',
+    styleUrls: ['./image-comparison.component.scss'],
 })
 export class ImageComparisonComponent implements OnInit {
+    @ViewChild('canvas', { static: true }) myCanvas!: ElementRef;
+    squareDimensions: number = 10;
+    ngOnInit(): void {
+        const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
+        const context = canvas.getContext('2d');
+        if (context) {
+            this.drawRectangle(context);
+        }
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    drawRectangle(context: CanvasRenderingContext2D): void {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        context.fillRect(100, 100, this.squareDimensions, this.squareDimensions);
+    }
 }
