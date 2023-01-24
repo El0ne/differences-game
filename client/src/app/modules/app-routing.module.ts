1f2a19853c8862de9706cc0f5cb876e539a139d4
Chahine <tchamitouche@gmail.com>
2023-01-24 16:38:10 -0500

    adding skeleton of page so I can angular features

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameCreationPageComponent } from '@app/pages/game-creation-page/game-creation-page.component';
 import { HomePageComponent } from '@app/pages/home-page/home-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
+    { path: 'creatingGame', component: GameCreationPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
