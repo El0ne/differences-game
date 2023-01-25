8836b47310674ee6613356e51d07bb12f339d494
Alexis Nicolas <alexisducanada@hotmail.com>
2023-01-24 22:27:26 -0500

    fixed tests not able to find the param of the route

--- client/src/app/pages/game-selection/game-selection.component.spec.ts
+++ client/src/app/pages/game-selection/game-selection.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { GAME_CARDS_TO_DISPLAY } from './game-selection-constants';
-
 import { GameSelectionComponent } from './game-selection.component';
 
 describe('GameSelectionComponent', () => {
@@ -11,6 +11,7 @@ describe('GameSelectionComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [GameSelectionComponent],
+            imports: [RouterTestingModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(GameSelectionComponent);
