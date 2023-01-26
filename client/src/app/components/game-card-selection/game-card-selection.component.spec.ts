6cc1c06c161222c7d8ae5e0918c85a3fec84ba78
Alexis Nicolas <alexisducanada@hotmail.com>
2023-01-25 23:52:40 -0500

    initialized gameCardInformation for testing game-card-selection

--- client/src/app/components/game-card-selection/game-card-selection.component.spec.ts
+++ client/src/app/components/game-card-selection/game-card-selection.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { game, GameCardInformation } from '@app/Classes/game-card';
 
 import { GameCardSelectionComponent } from './game-card-selection.component';
 
@@ -13,7 +14,10 @@ describe('GameCardSelectionComponent', () => {
 
         fixture = TestBed.createComponent(GameCardSelectionComponent);
         component = fixture.componentInstance;
+        component.gameCardInformation = new GameCardInformation();
         fixture.detectChanges();
+
+        component.gameCardInformation = game;
     });
 
     it('should create', () => {
