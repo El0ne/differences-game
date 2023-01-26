fea4285ac622a5cac2129599cdd6396914323f6a
Alexis Nicolas <alexisducanada@hotmail.com>
2023-01-26 10:43:22 -0500

    changed button press in tests for call to their methods

--- client/src/app/pages/game-selection/game-selection.component.spec.ts
+++ client/src/app/pages/game-selection/game-selection.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { GAME_CARDS_TO_DISPLAY } from './game-selection-constants';
 import { GameSelectionComponent } from './game-selection.component';
@@ -23,42 +22,57 @@ describe('GameSelectionComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('show-next-cards-button should put the end Index at 4 more than index until there is less than 4 other gameCards to show', () => {
+    it('selectGameCards() should put the endIndex at 4 more than index until there is less than 4 other gameCards to show', () => {
         component.endIndex = 4;
         component.numberOfGameInformations = 5;
         component.selectGameCards();
         expect(component.endIndex).toBe(component.index + GAME_CARDS_TO_DISPLAY);
-        fixture.debugElement.query(By.css('#show-next-cards-button')).triggerEventHandler('click', null);
+        component.nextCards();
         expect(component.endIndex).toBe(component.index + 1);
     });
 
-    it('show-previous-cards-button should not call selectGameCards() if index is 0', () => {
+    it('previousCards() should not call selectGameCards() if index is 0', () => {
         component.selectGameCards = jasmine.createSpy();
-        fixture.debugElement.query(By.css('#show-previous-cards-button')).triggerEventHandler('click', null);
+        component.previousCards();
         expect(component.selectGameCards).not.toHaveBeenCalled();
     });
 
-    it('show-previous-cards-button should call selectGameCards() if index is different than 0', () => {
+    it('previousCards() should call selectGameCards() if index is different than 0', () => {
         component.index = 4;
         component.endIndex = 5;
         component.selectGameCards = jasmine.createSpy();
-        fixture.debugElement.query(By.css('#show-previous-cards-button')).triggerEventHandler('click', null);
+        component.previousCards();
         expect(component.selectGameCards).toHaveBeenCalled();
     });
 
-    it('show-next-cards-button should not call selectGameCards() if EndIndex is equal to numberOfGameInformations', () => {
+    it('nextCards() should not call selectGameCards() if EndIndex is equal to numberOfGameInformations', () => {
         component.endIndex = 4;
-        component.numberOfGameInformations = 4;
+        component.numberOfGameInformations = component.endIndex;
         component.selectGameCards = jasmine.createSpy();
-        fixture.debugElement.query(By.css('#show-next-cards-button')).triggerEventHandler('click', null);
+        component.nextCards();
         expect(component.selectGameCards).not.toHaveBeenCalled();
     });
 
-    it('show-next-cards-button should call selectGameCards() if endIndex is different than numberOfGameInformations', () => {
+    it('nextCards() should call selectGameCards() if endIndex is different than numberOfGameInformations', () => {
         component.endIndex = 4;
         component.numberOfGameInformations = 5;
         component.selectGameCards = jasmine.createSpy();
-        fixture.debugElement.query(By.css('#show-next-cards-button')).triggerEventHandler('click', null);
+        component.nextCards();
         expect(component.selectGameCards).toHaveBeenCalled();
+    });
+
+    it('isShowingFirstCard() should return false unless index is 0', () => {
+        component.index = 4;
+        expect(component.isShowingFirstCard()).toBeFalsy();
+        component.index = 0;
+        expect(component.isShowingFirstCard()).toBeTruthy();
+    });
+
+    it('isShowingLastCard() should return false unless endIndex is the same as numberOfGameInformations', () => {
+        component.numberOfGameInformations = 5;
+        component.endIndex = 4;
+        expect(component.isShowingLastCard()).toBeFalsy();
+        component.endIndex = component.numberOfGameInformations;
+        expect(component.isShowingLastCard()).toBeTruthy();
     });
 });
