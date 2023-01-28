3bfd90073fb0a80231aebfbdbacddb1d7da64956
Chahine <tchamitouche@gmail.com>
2023-01-27 20:40:20 -0500

    adding upload feature

--- client/src/app/pages/game-creation-page/game-creation-page.component.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-game-creation-page',
     templateUrl: './game-creation-page.component.html',
     styleUrls: ['./game-creation-page.component.scss'],
 })
-export class GameCreationPageComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
+export class GameCreationPageComponent {
+    @ViewChild('myDiv') myInput: ElementRef;
+    url1 = '';
+    url2 = '';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    uploadOriginal(e: any) {
+        if (e.target.files) {
+            const reader = new FileReader();
+            reader.readAsDataURL(e.target.files[0]);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            reader.onload = (event: any) => {
+                this.url1 = event.target.result;
+            };
+        }
+    }
 }
