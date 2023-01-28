c026775a55991c0f03e016f0ec72fa09d3541014
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-01-27 21:49:39 -0500

    started cleaning code

--- server/app/services/difference-detection/difference-detection.service.ts
+++ server/app/services/difference-detection/difference-detection.service.ts
@@ -5,61 +5,58 @@ import * as Jimp from 'jimp';
 export class DifferenceDetectionService {
     width = 640;
     height = 480;
+    differenceArray: boolean[] = [];
 
-    batArray = [];
     pathToHere = './app/services/difference-detection/images';
-
-    hello(): void {
-        console.log('Hello World');
-    }
 
     async createArray(image: string): Promise<number[]> {
         const numArray: number[] = [];
         const img = await Jimp.read(`${this.pathToHere}/${image}`);
-        console.log('before image size', img.bitmap.data.length);
-
-        console.log('img.bitmap.height', img.bitmap.height);
-        console.log('img.bitmap.width', img.bitmap.width);
-        // img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-        //     numArray.push(this.bitmap.data[idx]);
-        // });
-        for (let i = 0; i < img.bitmap.data.length; i++) {
-            numArray.push(img.bitmap.data[i]);
-        }
-        console.log('numArray', numArray.length);
+        // console.log('before image size', img.bitmap.data.length);
+        // console.log('img.bitmap.height', img.bitmap.height);
+        // console.log('img.bitmap.width', img.bitmap.width);
+        for (const rgbColor of img.bitmap.data) numArray.push(rgbColor);
+        // console.log('numArray', numArray.length);
         return numArray;
     }
 
-    createDifferenceImage(differenceArray: boolean[]) {
+    createDifferenceImage() {
         const img = new Jimp(this.width, this.height, 'green', (err) => {
             if (err) throw err;
         });
-        console.log('after image size', img.bitmap.data.length);
-        img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-            if (differenceArray[idx] && differenceArray[idx + 1] && differenceArray[idx + 2] && differenceArray[idx + 3]) {
-                img.bitmap.data[idx] = 0xff;
-                img.bitmap.data[idx + 1] = 0xff;
-                img.bitmap.data[idx + 2] = 0xff;
-                // img.bitmap.data[idx + 3] = 0xff;
+        // console.log('after image size', img.bitmap.data.length);
+        img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+            if (this.arePixelSameColor(idx)) {
+                this.setPixelWhite(img, idx);
             } else {
-                img.bitmap.data[idx] = 0x00;
-                img.bitmap.data[idx + 1] = 0x00;
-                img.bitmap.data[idx + 2] = 0x00;
-                // img.bitmap.data[idx + 3] = 0xff;
+                this.setPixelBlack(img, idx);
             }
         });
         img.write(`${this.pathToHere}/difference-image.bmp`);
     }
 
+    arePixelSameColor(pixelIndex: number): boolean {
+        return this.differenceArray[pixelIndex] && this.differenceArray[pixelIndex + 1] && this.differenceArray[pixelIndex + 2];
+    }
+    setPixelWhite(image: Jimp, pixelIndex: number): void {
+        image.bitmap.data[pixelIndex] = 0xff;
+        image.bitmap.data[pixelIndex + 1] = 0xff;
+        image.bitmap.data[pixelIndex + 2] = 0xff;
+    }
+
+    setPixelBlack(image: Jimp, pixelIndex: number): void {
+        image.bitmap.data[pixelIndex] = 0x00;
+        image.bitmap.data[pixelIndex + 1] = 0x00;
+        image.bitmap.data[pixelIndex + 2] = 0x00;
+    }
     async compareImages() {
-        const base = await this.createArray('image_2_diff.bmp');
-        const modified = await this.createArray('image_12_diff.bmp');
-        const differenceArray: boolean[] = [];
+        const base = await this.createArray('image_12_diff.bmp');
+        const modified = await this.createArray('image_7_diff.bmp');
         for (let i = 0; i < base.length; i++) {
-            if (base[i] !== modified[i]) differenceArray.push(false);
-            else differenceArray.push(true);
+            if (base[i] !== modified[i]) this.differenceArray.push(false);
+            else this.differenceArray.push(true);
         }
-        console.log(differenceArray);
-        this.createDifferenceImage(differenceArray);
+        // console.log(this.differenceArray);
+        this.createDifferenceImage();
     }
 }
