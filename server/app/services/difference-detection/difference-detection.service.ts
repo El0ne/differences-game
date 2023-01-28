9b24d17c551f062363066dd1aa9d33da54f110de
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-01-27 21:36:19 -0500

    Image comparaison works for pixel arrays

--- server/app/services/difference-detection/difference-detection.service.ts
+++ server/app/services/difference-detection/difference-detection.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@nestjs/common';
 import * as Jimp from 'jimp';
 @Injectable()
 export class DifferenceDetectionService {
-    dimensions: number = 300;
+    width = 640;
+    height = 480;
 
     batArray = [];
     pathToHere = './app/services/difference-detection/images';
@@ -12,43 +13,49 @@ export class DifferenceDetectionService {
         console.log('Hello World');
     }
 
-    async createHexArray(image: string): Promise<number[]> {
+    async createArray(image: string): Promise<number[]> {
         const numArray: number[] = [];
         const img = await Jimp.read(`${this.pathToHere}/${image}`);
+        console.log('before image size', img.bitmap.data.length);
+
         console.log('img.bitmap.height', img.bitmap.height);
         console.log('img.bitmap.width', img.bitmap.width);
-        img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-            numArray.push(this.bitmap.data[idx]);
-        });
-        console.log('numArray', numArray);
+        // img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
+        //     numArray.push(this.bitmap.data[idx]);
+        // });
+        for (let i = 0; i < img.bitmap.data.length; i++) {
+            numArray.push(img.bitmap.data[i]);
+        }
+        console.log('numArray', numArray.length);
         return numArray;
     }
 
     createDifferenceImage(differenceArray: boolean[]) {
-        const img = new Jimp(this.dimensions, this.dimensions, 'green', (err) => {
+        const img = new Jimp(this.width, this.height, 'green', (err) => {
             if (err) throw err;
         });
+        console.log('after image size', img.bitmap.data.length);
         img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-            if (differenceArray[idx]) {
-                img.bitmap.data[idx] = 0x00;
-                img.bitmap.data[idx + 1] = 0x00;
-                img.bitmap.data[idx + 2] = 0x00;
-                img.bitmap.data[idx + 3] = 0xff;
-            } else {
+            if (differenceArray[idx] && differenceArray[idx + 1] && differenceArray[idx + 2] && differenceArray[idx + 3]) {
                 img.bitmap.data[idx] = 0xff;
                 img.bitmap.data[idx + 1] = 0xff;
                 img.bitmap.data[idx + 2] = 0xff;
-                img.bitmap.data[idx + 3] = 0xff;
+                // img.bitmap.data[idx + 3] = 0xff;
+            } else {
+                img.bitmap.data[idx] = 0x00;
+                img.bitmap.data[idx + 1] = 0x00;
+                img.bitmap.data[idx + 2] = 0x00;
+                // img.bitmap.data[idx + 3] = 0xff;
             }
         });
-        img.write(`${this.pathToHere}/difference-image.${img.getExtension()}`);
+        img.write(`${this.pathToHere}/difference-image.bmp`);
     }
 
     async compareImages() {
-        const base = await this.createHexArray('base.jpeg');
-        const modified = await this.createHexArray('modified.jpeg');
+        const base = await this.createArray('image_2_diff.bmp');
+        const modified = await this.createArray('image_12_diff.bmp');
         const differenceArray: boolean[] = [];
-        for (let i = 0; i < base.length; i += 4) {
+        for (let i = 0; i < base.length; i++) {
             if (base[i] !== modified[i]) differenceArray.push(false);
             else differenceArray.push(true);
         }
