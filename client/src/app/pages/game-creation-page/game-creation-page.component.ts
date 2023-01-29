d1f213aed9a8860ddec74312764223830ab96471
Chahine <tchamitouche@gmail.com>
2023-01-28 21:56:42 -0500

    working canvas

--- client/src/app/pages/game-creation-page/game-creation-page.component.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-game-creation-page',
@@ -6,10 +6,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./game-creation-page.component.scss'],
 })
 export class GameCreationPageComponent {
+    @ViewChild('canvas1') myOgCanvas: ElementRef;
+    @ViewChild('canvas2') myDiffCanvas: ElementRef;
     urlOriginal: string;
     urlDifferent: string;
 
     constructor() {}
+
+    ngOnInit(): void {
+        // const canvas = this.myCanvas.nativeElement;
+        // const context = canvas.getContext('2d');
+        // if (context) {
+        //     this.#drawRectangle(context);
+        // }
+    }
+
+    // #drawRectangle(context: CanvasRenderingContext2D) {
+    //     context.fillRect(20, 20, 100, 100);
+    // }
 
     clear(e: Event) {
         const target = e.target as HTMLInputElement;
@@ -25,6 +39,12 @@ export class GameCreationPageComponent {
     }
 
     fileValidation(e: Event) {
+        const ogCanvas: HTMLCanvasElement = this.myOgCanvas.nativeElement;
+        const diffCanvas: HTMLCanvasElement = this.myDiffCanvas.nativeElement;
+
+        const ogContext = ogCanvas.getContext('2d');
+        const diffContext = diffCanvas.getContext('2d');
+
         const target = e.target as HTMLInputElement;
         const file: File = (target.files as FileList)[0];
         if (file !== undefined && file.size === 921654 && file.type === 'image/bmp') {
@@ -39,19 +59,29 @@ export class GameCreationPageComponent {
                     } else {
                         switch (target.id) {
                             case 'upload-original': {
-                                this.urlOriginal = reader.result as string;
-
+                                // this.urlOriginal = reader.result as string;
+                                if (ogContext) {
+                                    ogContext.drawImage(img, 0, 0, 640, 480);
+                                }
                                 break;
                             }
                             case 'upload-different': {
-                                this.urlDifferent = reader.result as string;
+                                // this.urlDifferent = reader.result as string;
+                                if (diffContext) {
+                                    diffContext.drawImage(img, 0, 0, 640, 480);
+                                }
 
                                 break;
                             }
                             case 'upload-both': {
-                                this.urlOriginal = reader.result as string;
-                                this.urlDifferent = reader.result as string;
-
+                                // this.urlOriginal = reader.result as string;
+                                // this.urlDifferent = reader.result as string;
+                                if (ogContext) {
+                                    ogContext.drawImage(img, 0, 0, 640, 480);
+                                }
+                                if (diffContext) {
+                                    diffContext.drawImage(img, 0, 0, 640, 480);
+                                }
                                 break;
                             }
                             // No default
@@ -66,5 +96,4 @@ export class GameCreationPageComponent {
             target.value = '';
         }
     }
-    ngOnInit(): void {}
 }
