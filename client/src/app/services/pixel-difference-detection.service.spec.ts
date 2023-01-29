f22d028722a17471d4bc503d07df61b77419633c
Sam <sam.younan@polymtl.ca>
2023-01-29 16:02:53 -0500

    Set up the pixel radius and tested it sucessfully.

--- client/src/app/services/pixel-difference-detection.service.spec.ts
+++ client/src/app/services/pixel-difference-detection.service.spec.ts
@@ -13,4 +13,16 @@ describe('PixelDifferenceDetectionService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('should return an array of pixels around a pixel', () => {
+        const pixelCoordinateX = 5;
+        const pixelCoordinateY = 5;
+        const radiusSize = 2;
+        const expectedPixelsAroundMyPixel = [
+            3, 3, 3, 4, 3, 5, 3, 6, 3, 7, 4, 3, 4, 4, 4, 5, 4, 6, 4, 7, 5, 3, 5, 4, 5, 5, 5, 6, 5, 7, 6, 3, 6, 4, 6, 5, 6, 6, 6, 7, 7, 3, 7, 4, 7, 5,
+            7, 6, 7, 7,
+        ];
+        const returnedPixelsAroundMyPixel = service.getPixelsAroundPixel(pixelCoordinateX, pixelCoordinateY, radiusSize);
+        expect(returnedPixelsAroundMyPixel).toEqual(expectedPixelsAroundMyPixel);
+    });
 });
--- client/src/app/services/pixel-difference-detection.service.ts
+++ client/src/app/services/pixel-difference-detection.service.ts
@@ -5,4 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class PixelDifferenceDetectionService {
     constructor() {}
+
+    // function to get all the pixels directly around a pixel with a radius of radiusSize
+    getPixelsAroundPixel(pixelCoordinateX: number, pixelCoordinateY: number, radiusSize: number): number[] {
+        const pixelsAroundMyPixel: number[] = [];
+        for (let i = pixelCoordinateX - radiusSize; i <= pixelCoordinateX + radiusSize; i++) {
+            for (let j = pixelCoordinateY - radiusSize; j <= pixelCoordinateY + radiusSize; j++) {
+                pixelsAroundMyPixel.push(i, j);
+            }
+        }
+        return pixelsAroundMyPixel;
+    }
 }
