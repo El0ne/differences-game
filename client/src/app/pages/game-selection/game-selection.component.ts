cb7c407c9a0c136ecb7ea1fc8a587ad59ae72981
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-01-29 10:53:10 -0500

    Fixed next and previous buttons issue and added other game to test visually

--- client/src/app/pages/game-selection/game-selection.component.ts
+++ client/src/app/pages/game-selection/game-selection.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { game, GameCardInformation } from '@app/Classes/game-card';
+import { game, game2, GameCardInformation } from '@app/Classes/game-card';
 import { GameOrConfigSelectionService } from '@app/services/game-or-config-selection/game-or-config-selection.service';
 import { GAME_CARDS_TO_DISPLAY } from './game-selection-constants';
 
@@ -9,8 +9,8 @@ import { GAME_CARDS_TO_DISPLAY } from './game-selection-constants';
     styleUrls: ['./game-selection.component.scss'],
 })
 export class GameSelectionComponent implements OnInit {
-    gameCardInformations: GameCardInformation[] = [game, game, game, game, game, game]; // TODO vider lorsque la BD est implementee
-    numberOfGameInformations = 0;
+    gameCardInformations: GameCardInformation[] = [game, game, game, game2, game, game2]; // TODO vider lorsque la BD est implementee
+    numberOfGameInformations = this.gameCardInformations.length;
     index: number = 0;
     endIndex: number = 0;
     isConfig: boolean | null;
@@ -36,6 +36,7 @@ export class GameSelectionComponent implements OnInit {
             this.index += GAME_CARDS_TO_DISPLAY;
             this.selectGameCards();
         }
+        console.log('nextCard called');
     }
 
     previousCards(): void {
@@ -47,10 +48,12 @@ export class GameSelectionComponent implements OnInit {
     }
 
     isShowingFirstCard(): boolean {
+        console.log('this.index === 0', this.index === 0);
         return this.index === 0;
     }
 
     isShowingLastCard(): boolean {
+        console.log('this.index === this.numberOfGameInformations', this.index === this.numberOfGameInformations);
         return this.endIndex === this.numberOfGameInformations;
     }
 }
