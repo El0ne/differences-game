19f9fe7343c16cac2c543262029d0f45ed4878e1
Alexis Nicolas <alexisducanada@hotmail.com>
2023-01-30 14:29:14 -0500

    optimized code for difference generation

--- server/app/services/difference-detection/difference-detection.service.ts
+++ server/app/services/difference-detection/difference-detection.service.ts
@@ -7,7 +7,7 @@ export class DifferenceDetectionService {
     // TODO Remove images when I receive array from soloGameView
     firstImageArray: number[];
     secondImageArray: number[];
-    firstImage: string = 'image_12_diff.bmp';
+    firstImage: string = 'image_7_diff.bmp';
     secondImage: string = 'image_2_diff.bmp';
     t0: number;
     t1: number;
@@ -34,19 +34,13 @@ export class DifferenceDetectionService {
             if (this.isSamePixelColor(i)) this.differenceArray.push(true);
             else this.differenceArray.push(false);
         }
-        // console.log('this.differenceArray', this.differenceArray);
-        // this.testSmallerArrayBool(this.testArray);
         this.createDifferenceImage();
         this.t1 = performance.now();
         console.log('this.t1 - this.t0', this.t1 - this.t0);
     }
 
     isSamePixelColor(index: number) {
-        return (
-            this.firstImageArray[index] === this.secondImageArray[index] &&
-            this.firstImageArray[index + 1] === this.secondImageArray[index + 2] &&
-            this.firstImageArray[index + 2] === this.secondImageArray[index + 2]
-        );
+        return this.firstImageArray[index] === this.secondImageArray[index];
     }
 
     createDifferenceImage(): void {
@@ -63,44 +57,24 @@ export class DifferenceDetectionService {
             }
         });
         console.log('this.bfsArray.length', this.bfsArray.length);
-
-        // for (let i = 0; i < this.differenceArray.length; i++) {
-        //     // *4 i+=4
-        //     if (this.arePixelSameRGB(i)) {
-        //         this.setPixelWhite(image, i);
-        //     } else {
-        //         this.setPixelBlack(image, i);
-        //     }
-        // }
-
+        console.log('differenceArray', this.differenceArray.length);
         image.write(`${PATH_TO_IMAGES}/difference-image.bmp`);
     }
 
-    // testSmallerArrayBool(testArray) {
-    //     const boolArray: boolean[] = [];
-    //     for (let i = 0; i < testArray.length; i += 4) {
-    //         boolArray.push(this.arePixelTest(i, testArray));
-    //     }
-    //     console.log('boolArray', boolArray);
-    //     return boolArray;
-    // }
-
-    arePixelTest(pixelIndex: number, testArray): boolean {
-        return testArray[pixelIndex] && testArray[pixelIndex + 1] && testArray[pixelIndex + 2];
-    }
-
     arePixelSameRGB(pixelIndex: number): boolean {
-        return this.differenceArray[pixelIndex] && this.differenceArray[pixelIndex + 1] && this.differenceArray[pixelIndex + 2];
+        return this.differenceArray[pixelIndex];
     }
     setPixelWhite(image: Jimp, pixelIndex: number): void {
         image.bitmap.data[pixelIndex] = 0xff;
         image.bitmap.data[pixelIndex + 1] = 0xff;
         image.bitmap.data[pixelIndex + 2] = 0xff;
+        image.bitmap.data[pixelIndex + 3] = 0xff;
     }
 
     setPixelBlack(image: Jimp, pixelIndex: number): void {
         image.bitmap.data[pixelIndex] = 0x00;
         image.bitmap.data[pixelIndex + 1] = 0x00;
         image.bitmap.data[pixelIndex + 2] = 0x00;
+        image.bitmap.data[pixelIndex + 3] = 0xff;
     }
 }
