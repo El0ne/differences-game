213c37290d5e203d897fa9d39eb2e6c0ac9822d2
JulienLegare <julien.legare@polymtl.ca>
2023-01-30 12:09:34 -0500

    tests for the timer

--- client/src/app/services/timer-solo/timer-solo.service.spec.ts
+++ client/src/app/services/timer-solo/timer-solo.service.spec.ts
@@ -1,6 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-
-import { TimerSoloService } from '../../timer-solo.service';
+import { discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TimerSoloService } from './timer-solo.service';
 
 describe('TimerSoloService', () => {
     let service: TimerSoloService;
@@ -13,4 +12,11 @@ describe('TimerSoloService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('should increment 10 seconds after 10 seconds properly', fakeAsync(() => {
+        service.startTimer();
+        tick(10000);
+        discardPeriodicTasks();
+        expect(service.currTime).toEqual(10);
+    }));
 });
