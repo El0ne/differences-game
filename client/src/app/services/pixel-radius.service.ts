843d1a50ac20601a4b787c3de8ded810d5668911
Sam <sam.younan@polymtl.ca>
2023-01-30 14:37:15 -0500

    pixel radius works (2d and 1d)

---
+++ client/src/app/services/pixel-radius.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PixelRadiusService {
+    constructor() {}
+
+    getPixelsAroundPixel(pixelCoordinateX: number, pixelCoordinateY: number, radiusSize: number): number[] {
+        const pixelsAroundMyPixel: number[] = [];
+        for (let i = pixelCoordinateX - radiusSize; i <= pixelCoordinateX + radiusSize; i++) {
+            for (let j = pixelCoordinateY - radiusSize; j <= pixelCoordinateY + radiusSize; j++) {
+                pixelsAroundMyPixel.push(i, j);
+            }
+        }
+        return pixelsAroundMyPixel;
+    }
+
+    getPixelsAroundPixelInOneDimensionalArray(pixelLocation: number, radiusSize: number): number[] {
+        const imageWidth = 640;
+        const imageHeight = 480;
+        const pixelCoordinateX = pixelLocation % imageWidth;
+        const pixelCoordinateY = Math.floor(pixelLocation / imageWidth);
+        const pixelsAroundMyPixel: number[] = [];
+
+        const leftExtremity = Math.max(pixelCoordinateX - radiusSize, 0);
+        const rightExtremity = Math.min(pixelCoordinateX + radiusSize, imageWidth);
+        const downExtremity = Math.max(pixelCoordinateY - radiusSize, 0);
+        const upExtremity = Math.min(pixelCoordinateY + radiusSize, imageHeight);
+
+        for (let j = downExtremity; j <= upExtremity; j++) {
+            for (let i = leftExtremity; i <= rightExtremity; i++) {
+                pixelsAroundMyPixel.push(j * imageWidth + i);
+            }
+        }
+
+        return pixelsAroundMyPixel;
+    }
+}
