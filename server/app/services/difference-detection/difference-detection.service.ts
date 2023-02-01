48bb03eb18e8b37f6113fce3e901c54f9bd8f631
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-01 14:57:49 -0500

    Optimized code a bit (about 15-20% faster)

--- server/app/services/difference-detection/difference-detection.service.ts
+++ server/app/services/difference-detection/difference-detection.service.ts
@@ -7,7 +7,7 @@ export class DifferenceDetectionService {
     // TODO Remove images when I receive array from soloGameView
     firstImageArray: number[];
     secondImageArray: number[];
-    firstImage: string = 'image_7_diff.bmp';
+    firstImage: string = 'image_12_diff.bmp';
     secondImage: string = 'image_2_diff.bmp';
     t0: number;
     t1: number;
@@ -30,11 +30,12 @@ export class DifferenceDetectionService {
         this.firstImageArray = await this.createArray(this.firstImage);
         this.secondImageArray = await this.createArray(this.secondImage);
         this.t0 = performance.now();
-        for (let i = 0; i < this.firstImageArray.length; i++) {
-            if (this.isSamePixelColor(i)) this.differenceArray.push(true);
-            else this.differenceArray.push(false);
-        }
-        this.createDifferenceImage();
+        // for (let i = 0; i < this.firstImageArray.length; i++) {
+        //     if (this.isSamePixelColor(i)) this.differenceArray.push(true);
+        //     else this.differenceArray.push(false);
+        // }
+        // this.createDifferenceImage();
+        this.createDifferenceImage2();
         this.t1 = performance.now();
         console.log('this.t1 - this.t0', this.t1 - this.t0);
     }
@@ -49,6 +50,24 @@ export class DifferenceDetectionService {
         });
         image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
             if (this.arePixelSameRGB(idx)) {
+                this.setPixelWhite(image, idx);
+                this.bfsArray.push(true);
+            } else {
+                this.setPixelBlack(image, idx);
+                this.bfsArray.push(false);
+            }
+        });
+        console.log('this.bfsArray.length', this.bfsArray.length);
+        console.log('differenceArray', this.differenceArray.length);
+        image.write(`${PATH_TO_IMAGES}/difference-image.bmp`);
+    }
+
+    createDifferenceImage2(): void {
+        const image = new Jimp(IMAGE_WIDTH, IMAGE_HEIGHT, 'white', (err) => {
+            if (err) throw err;
+        });
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+            if (this.test(idx)) {
                 this.setPixelWhite(image, idx);
                 this.bfsArray.push(true);
             } else {
@@ -76,5 +95,14 @@ export class DifferenceDetectionService {
         image.bitmap.data[pixelIndex + 1] = 0x00;
         image.bitmap.data[pixelIndex + 2] = 0x00;
         image.bitmap.data[pixelIndex + 3] = 0xff;
+    }
+
+    test(pixelIndex: number): boolean {
+        return (
+            this.firstImageArray[pixelIndex] === this.secondImageArray[pixelIndex] &&
+            this.firstImageArray[pixelIndex + 1] === this.secondImageArray[pixelIndex + 1] &&
+            this.firstImageArray[pixelIndex + 2] === this.secondImageArray[pixelIndex + 2] &&
+            this.firstImageArray[pixelIndex + 3] === this.secondImageArray[pixelIndex + 3]
+        );
     }
 }
