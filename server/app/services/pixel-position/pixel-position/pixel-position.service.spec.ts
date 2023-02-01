5e8160d6114602c22cd7898de7f0826f010dac19
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-01 18:28:57 -0500

    fixed linting

--- server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
+++ server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PixelPositionService } from './pixel-position.service';
 
 describe('PixelPositionService', () => {
-  let service: PixelPositionService;
+    let service: PixelPositionService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PixelPositionService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [PixelPositionService],
+        }).compile();
 
-    service = module.get<PixelPositionService>(PixelPositionService);
-  });
+        service = module.get<PixelPositionService>(PixelPositionService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
