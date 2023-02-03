b823d3681d50adfc5c8a650bf63e1d65c1ece99e
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-02 21:41:41 -0500

    clean code

--- server/app/services/game-card/game-card.service.ts
+++ server/app/services/game-card/game-card.service.ts
@@ -6,17 +6,7 @@ import * as path from 'path';
 export class GameCardService {
     async getGameCards(startIndex: number, endIndex: number): Promise<GameCardInformation[]> {
         const dataPath = path.join(process.cwd(), '/app/dataBase/game-cards-informations.json');
-        // const test = await fs.readFile(dataPath, 'utf-8', (error, data) => {
-        //     if (error) {
-        //         console.log(error);
-        //         return;
-        //     }
-        //     console.log('data', data);
-        //     return JSON.parse(data);
-        // });
-        // console.log('test', test);
         const content = fs.readFileSync(dataPath, 'utf8');
-        console.log(JSON.parse(content).gameCardsInformations.slice(startIndex, endIndex));
         return JSON.parse(content).gameCardsInformations.slice(startIndex, endIndex);
     }
 }
