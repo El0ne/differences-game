b1d640610bd506329c16dabb988302d669a877c4
Alexis Nicolas <alexisducanada@hotmail.com>
2023-02-03 17:03:47 -0500

    stored patch of differences detected by bfs in an array of array

--- server/app/services/differences-detection/differences-detection/differences-detection.service.ts
+++ server/app/services/differences-detection/differences-detection/differences-detection.service.ts
@@ -1,36 +1,48 @@
+import { PixelRadiusService } from '@app/services/pixel-radius/pixel-radius.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DifferencesDetectionService {
-    async getNumberOfDifferences(differencesArray: boolean[]): Promise<boolean[]> {
-        const visitedPixels: number[] = [];
-        const queue: number[];
+    constructor(private pixelRadiusService: PixelRadiusService) {}
+
+    async getNumberOfDifferences(differencesArray: boolean[]): Promise<number[][]> {
+        const visitedPixels: number[][] = [];
+        const queue: number[] = [];
         let numberOfDifferences = 0;
 
         differencesArray.forEach(async (different, index) => {
             if (this.isNewAndDifferentPixel(index, differencesArray, visitedPixels)) {
                 queue.push(index);
-                visitedPixels.push(index);
+                visitedPixels.push([index]);
                 numberOfDifferences++;
 
                 const currentPos = queue.shift();
                 while (queue.length !== 0) {
-                    for (const adjacentPixel of await this.adjacentPixels(currentPos)) {
+                    for (const adjacentPixel of await this.pixelRadiusService.getAdjacentPixels(currentPos, 1)) {
                         if (this.isNewAndDifferentPixel(adjacentPixel, differencesArray, visitedPixels)) {
-                            visitedPixels.push(adjacentPixel);
+                            visitedPixels[numberOfDifferences].push(adjacentPixel);
                             queue.push(adjacentPixel);
                         }
                     }
                 }
             }
         });
+
+        return visitedPixels;
     }
 
-    async adjacentPixels(currentIndex: number): Promise<number[]> {
-        return [];
+    isNewAndDifferentPixel(index: number, differencesArray: boolean[], visitedPixels: number[][]) {
+        return differencesArray[index] && !this.isAVisitedPixel(index, visitedPixels);
     }
 
-    isNewAndDifferentPixel(index: number, differencesArray: boolean[], visitedPixels: number[]) {
-        return differencesArray[index] && !visitedPixels.includes(index);
+    findPixelDifferenceIndex(pixelIndex: number, visitedPixels: number[][]): number /* usable for the game to find which pixel was cliqued*/ {
+        visitedPixels.forEach((difference, differenceIndex) => {
+            if (difference.includes(pixelIndex)) return differenceIndex;
+        });
+        return visitedPixels.length;
+    }
+
+    isAVisitedPixel(pixelIndex: number, visitedPixels: number[][]): boolean {
+        return this.findPixelDifferenceIndex(pixelIndex, visitedPixels) === visitedPixels.length;
     }
 }
