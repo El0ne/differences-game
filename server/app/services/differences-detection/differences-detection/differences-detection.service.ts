b35f51211befe095e3cebe68ea61f47dbdb4924d
Alexis Nicolas <alexisducanada@hotmail.com>
2023-02-03 00:16:23 -0500

    started to write bfs, number of differences found implemented not tested

--- server/app/services/differences-detection/differences-detection/differences-detection.service.ts
+++ server/app/services/differences-detection/differences-detection/differences-detection.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DifferencesDetectionService {}
+export class DifferencesDetectionService {
+    async getNumberOfDifferences(differencesArray: boolean[]): Promise<boolean[]> {
+        const visitedPixels: number[] = [];
+        const queue: number[];
+        let numberOfDifferences = 0;
+
+        differencesArray.forEach(async (different, index) => {
+            if (this.isNewAndDifferentPixel(index, differencesArray, visitedPixels)) {
+                queue.push(index);
+                visitedPixels.push(index);
+                numberOfDifferences++;
+
+                const currentPos = queue.shift();
+                while (queue.length !== 0) {
+                    for (const adjacentPixel of await this.adjacentPixels(currentPos)) {
+                        if (this.isNewAndDifferentPixel(adjacentPixel, differencesArray, visitedPixels)) {
+                            visitedPixels.push(adjacentPixel);
+                            queue.push(adjacentPixel);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    async adjacentPixels(currentIndex: number): Promise<number[]> {
+        return [];
+    }
+
+    isNewAndDifferentPixel(index: number, differencesArray: boolean[], visitedPixels: number[]) {
+        return differencesArray[index] && !visitedPixels.includes(index);
+    }
+}
