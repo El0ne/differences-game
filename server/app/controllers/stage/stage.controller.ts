0597b3d63954f83dd0258d07296f24c9886ba8a3
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-02 21:48:11 -0500

    Added function to get number of games

--- server/app/controllers/stage/stage.controller.ts
+++ server/app/controllers/stage/stage.controller.ts
@@ -1,4 +1,3 @@
-import { GAMES } from '@app/dataBase/stages';
 import { GameCardService } from '@app/services/game-card/game-card.service';
 import { GameCardInformation } from '@common/game-card';
 import { Controller, Get, Query } from '@nestjs/common';
@@ -7,14 +6,12 @@ import { Controller, Get, Query } from '@nestjs/common';
 export class StageController {
     constructor(private gameCardService: GameCardService) {}
     @Get()
-    async test(@Query('index') index: number, @Query('endIndex') endIndex: number): Promise<GameCardInformation[]> {
-        console.log('index', index);
-        console.log('endIndex', endIndex);
-        return await this.gameCardService.getGameCards(index, endIndex);
+    test(@Query('index') index: number, @Query('endIndex') endIndex: number): GameCardInformation[] {
+        return this.gameCardService.getGameCards(index, endIndex);
     }
 
     @Get('info')
     getNbOfStages(): number {
-        return GAMES.length;
+        return this.gameCardService.getGameCardsNumber();
     }
 }
--- server/app/services/game-card/game-card.service.ts
+++ server/app/services/game-card/game-card.service.ts
@@ -4,9 +4,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 @Injectable()
 export class GameCardService {
-    async getGameCards(startIndex: number, endIndex: number): Promise<GameCardInformation[]> {
-        const dataPath = path.join(process.cwd(), '/app/dataBase/game-cards-informations.json');
-        const content = fs.readFileSync(dataPath, 'utf8');
+    jsonPath = path.join(process.cwd(), '/app/dataBase/game-cards-informations.json');
+    getGameCards(startIndex: number, endIndex: number): GameCardInformation[] {
+        const content = fs.readFileSync(this.jsonPath, 'utf8');
         return JSON.parse(content).gameCardsInformations.slice(startIndex, endIndex);
+    }
+    getGameCardsNumber(): number {
+        const content = fs.readFileSync(this.jsonPath, 'utf8');
+        return JSON.parse(content).gameCardsInformations.length;
     }
 }
