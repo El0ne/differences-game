00e3bc974b68f11961bdbaa0671f56f1f657dfd6
JulienLegare <julien.legare@polymtl.ca>
2023-02-04 12:48:48 -0500

    click on difference working, but slow

--- client/src/app/components/click-event/click-event.component.ts
+++ client/src/app/components/click-event/click-event.component.ts
@@ -12,6 +12,7 @@ export class ClickEventComponent implements OnInit {
     @Input() id: number;
     @Input() original: string;
     timeout: boolean;
+    lastDifferenceClicked: number[];
 
     constructor() {}
 
@@ -30,26 +31,55 @@ export class ClickEventComponent implements OnInit {
     getCoordInImage(e: MouseEvent): number[] {
         const canvas = e.target as HTMLCanvasElement;
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor(e.clientX - rect.left);
-        const y = Math.floor(e.clientY - rect.top);
+        let x = Math.floor(e.clientX - rect.left);
+        if (x < 0) {
+            x = 0;
+        }
+        let y = Math.floor(e.clientY - rect.top);
+        if (y < 0) {
+            y = 0;
+        }
         return new Array(x, y);
     }
 
     isDifferent(e: MouseEvent, differenceArray: number[][]): boolean {
-        if (!this.isADifference(differenceArray, this.getCoordInImage(e)[0], this.getCoordInImage(e)[1])) {
-            this.emitSound();
-            return false;
-        } else {
-            console.log('not ding');
+        if (this.isADifference(differenceArray, this.getCoordInImage(e)[0], this.getCoordInImage(e)[1])) {
+            this.differenceEffect(e);
             return true;
+        } else {
+            return false;
         }
     }
 
-    isADifference(array: number[][], x: number, y: number) {
+    differenceEffect(e: MouseEvent) {
+        const canvas = e.target as HTMLCanvasElement;
+        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.emitSound();
+        for (const pixel of this.lastDifferenceClicked) {
+            const pos: number[] = this.positionToPixel(pixel);
+            context.fillStyle = '#F00';
+            context.fillRect(pos[0], pos[1], 1, 1);
+        }
+    }
+
+    positionToPixel(toTransform: number) {
+        let yCounter = 0;
+
+        while (toTransform >= 640) {
+            toTransform -= 640;
+            if (toTransform >= 0) {
+                yCounter += 1;
+            }
+        }
+        return [toTransform, yCounter];
+    }
+
+    isADifference(array: number[][], x: number, y: number): boolean {
         const posToCheck = y * 640 + x;
         for (const difference of array) {
             for (const positions of difference) {
                 if (positions === posToCheck) {
+                    this.lastDifferenceClicked = difference;
                     return true;
                 }
             }
@@ -57,15 +87,14 @@ export class ClickEventComponent implements OnInit {
         return false;
     }
 
-    emitSound() {
+    emitSound(): void {
         const sound = new Audio('/assets/ding.mp3');
         sound.play();
     }
 
-    displayError(e: MouseEvent) {
+    displayError(e: MouseEvent): void {
         if (this.timeout) {
-            const error = this.isDifferent(e, this.differenceArray);
-            if (error) {
+            if (!this.isDifferent(e, this.differenceArray)) {
                 this.timeout = false;
                 const canvas = e.target as HTMLCanvasElement;
                 const rect = canvas.getBoundingClientRect();
