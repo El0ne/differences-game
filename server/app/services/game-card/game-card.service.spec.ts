10308c2abd4498efa95635f2d10bf20582911883
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-04 09:29:38 -0500

    Added tests for gameCardServices. Changed tsConfig to allow imports from json file

--- server/app/services/game-card/game-card.service.spec.ts
+++ server/app/services/game-card/game-card.service.spec.ts
@@ -1,6 +1,6 @@
+import { gameCardsInformations } from '@app/dataBase/game-cards-informations.json';
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameCardService } from './game-card.service';
-
 describe('GameCardService', () => {
     let service: GameCardService;
 
@@ -14,5 +14,17 @@ describe('GameCardService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('getGameCards should return all gameCards informations between both indexes', async () => {
+        const startIndex = 2;
+        const endIndex = 6;
+        const gameCards = service.getGameCards(startIndex, endIndex);
+        expect(gameCards).toEqual(gameCardsInformations.slice(startIndex, endIndex));
+    });
+
+    it('getGameCardsNumber should return the number of gameCards informations we have', async () => {
+        const gameCardsNumber = service.getGameCardsNumber();
+        expect(gameCardsNumber).toEqual(gameCardsInformations.length);
     });
 });
