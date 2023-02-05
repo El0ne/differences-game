3b86131aabb7a1ed9365b3b65616113ef5da56de
Sam <sam.younan@polymtl.ca>
2023-02-05 12:11:58 -0500

    bug du pixel de coin réglé, ordre de parcours inversé, test supplémentaire ajouté

--- client/src/app/services/pixel-radius-service/pixel-radius.service.ts
+++ client/src/app/services/pixel-radius-service/pixel-radius.service.ts
@@ -11,11 +11,11 @@ export class PixelRadiusService {
         const pixelsAroundMyPixel: number[] = [];
 
         const leftExtremity = Math.max(pixelCoordinateX - radiusSize, 0);
-        const rightExtremity = Math.min(pixelCoordinateX + radiusSize, IMAGE_WIDTH);
-        const downExtremity = Math.max(pixelCoordinateY - radiusSize, 0);
-        const upExtremity = Math.min(pixelCoordinateY + radiusSize, IMAGE_HEIGHT);
+        const rightExtremity = Math.min(pixelCoordinateX + radiusSize, IMAGE_WIDTH - 1);
+        const upExtremity = Math.max(pixelCoordinateY - radiusSize, 0);
+        const downExtremity = Math.min(pixelCoordinateY + radiusSize, IMAGE_HEIGHT - 1);
 
-        for (let j = downExtremity; j <= upExtremity; j++) {
+        for (let j = upExtremity; j <= downExtremity; j++) {
             for (let i = leftExtremity; i <= rightExtremity; i++) {
                 pixelsAroundMyPixel.push(j * IMAGE_WIDTH + i);
             }
