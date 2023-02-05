07c2a743fbf889072c52193a160a54dc1f8711da
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-05 11:04:11 -0500

    clean code

--- server/app/controllers/stage/stage.controller.ts
+++ server/app/controllers/stage/stage.controller.ts
@@ -3,31 +3,8 @@ import { GameCardInformation } from '@common/game-card';
 import { Controller, Get, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
-import path = require('path');
-const multer = require('multer');
-// based on https://www.youtube.com/watch?v=f-URVd2OKYc
-// export const storage = {
-//     storage: diskStorage({
-//         destination: './assets/images',
-//         filename: (req, file, cb) => {
-//             const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
-//             const extension: string = path.parse(file.originalname).ext;
-//             cb(null, `${filename}${extension}`);
-//         },
-//     }),
-// };
-
-// export const upload = multer({
-//     storage,
-//     fileFilter: (req, file, cb) => {
-//         if (file.mimetype === 'image/bmp') {
-//             cb(null, true);
-//         } else {
-//             cb(null, false);
-//         }
-//     },
-// });
 
 @Controller('stage')
 export class StageController {
@@ -50,6 +27,7 @@ export class StageController {
                 { name: 'gameData', maxCount: 1 },
             ],
             {
+                // based on https://www.youtube.com/watch?v=f-URVd2OKYc
                 storage: diskStorage({
                     destination: './assets/images',
                     filename: (req, file, cb) => {
@@ -59,8 +37,7 @@ export class StageController {
                     },
                 }),
                 fileFilter: (req, file, cb) => {
-                    // Check if the file should be accepted or not
-                    if (file.fieldname !== 'gameData') {
+                    if (file.mimetype === 'image/bmp') {
                         return cb(null, true);
                     }
                     return cb(null, false);
