55d42e33096c3dc68f51d0c9ece20528b0ae22c9
Alexis Nicolas <alexisducanada@hotmail.com>
2023-02-06 11:17:10 -0500

    moved test constants in test file

--- server/app/services/pixel-radius/pixel-radius.service.spec.ts
+++ server/app/services/pixel-radius/pixel-radius.service.spec.ts
@@ -1,15 +1,24 @@
-import {
-    EXPECTED_PIXELS_WITH_RADIUS_OF_0,
-    EXPECTED_PIXELS_WITH_RADIUS_OF_1,
-    EXPECTED_PIXELS_WITH_RADIUS_OF_2,
-    EXPECTED_PIXELS_WITH_RADIUS_OF_3,
-} from '@app/services/constants/pixel.constants';
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { ImageDimentionsService } from '@app/services/image-dimentions/image-dimentions.service';
 import { PixelPositionService } from '@app/services/pixel-position/pixel-position/pixel-position.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PixelRadiusService } from './pixel-radius.service';
 describe('PixelRadiusService', () => {
     let service: PixelRadiusService;
+
+    // radius test 1
+    const EXPECTED_PIXELS_WITH_RADIUS_OF_1 = [4, 5, 6, 644, 645, 646, 1284, 1285, 1286];
+    // radius test 2
+    const EXPECTED_PIXELS_WITH_RADIUS_OF_0 = [27];
+    // radius test 3
+    const EXPECTED_PIXELS_WITH_RADIUS_OF_3 = [
+        0, 1, 2, 3, 4, 5, 640, 641, 642, 643, 644, 645, 1280, 1281, 1282, 1283, 1284, 1285, 1920, 1921, 1922, 1923, 1924, 1925, 2560, 2561, 2562,
+        2563, 2564, 2565,
+    ];
+    // radius test 4
+    const EXPECTED_PIXELS_WITH_RADIUS_OF_2 = [
+        305276, 305277, 305278, 305279, 305916, 305917, 305918, 305919, 306556, 306557, 306558, 306559, 307196, 307197, 307198, 307199,
+    ];
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
