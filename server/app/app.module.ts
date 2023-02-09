6c7e967ff13e58a1baab7964f3aa8f5ceb0e7756
Alexis Nicolas <alexisducanada@hotmail.com>
2023-02-09 10:20:41 -0500

    added all providers in app.module

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -7,7 +7,11 @@ import { DateService } from '@app/services/date/date.service';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DifferenceDetectionService } from './services/difference-detection/difference-detection.service';
+import { DifferencesCounterService } from './services/differences-counter/differences-counter.service';
+import { ImageDimensionsService } from './services/image-dimensions/image-dimensions.service';
 import { PixelPositionService } from './services/pixel-position/pixel-position/pixel-position.service';
+import { PixelRadiusService } from './services/pixel-radius/pixel-radius.service';
 
 @Module({
     imports: [
@@ -22,6 +26,16 @@ import { PixelPositionService } from './services/pixel-position/pixel-position/p
         MongooseModule.forFeature([{ name: Course.name, schema: courseSchema }]),
     ],
     controllers: [CourseController, DateController],
-    providers: [ChatGateway, CourseService, DateService, Logger, PixelPositionService],
+    providers: [
+        ChatGateway,
+        CourseService,
+        DateService,
+        Logger,
+        PixelPositionService,
+        ImageDimensionsService,
+        DifferenceDetectionService,
+        DifferencesCounterService,
+        PixelRadiusService,
+    ],
 })
 export class AppModule {}
