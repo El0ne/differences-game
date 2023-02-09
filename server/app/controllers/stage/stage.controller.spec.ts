90ffa35968f5efb179a86c39ba768691a060d39e
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-09 18:55:56 -0500

    test without jimp just for fun

--- server/app/controllers/stage/stage.controller.spec.ts
+++ server/app/controllers/stage/stage.controller.spec.ts
@@ -6,8 +6,7 @@ import { GameInformation } from '@common/game-information';
 import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { assert } from 'console';
-import * as fs from 'fs';
-import * as Jimp from 'jimp';
+// import * as Jimp from 'jimp';
 import * as path from 'path';
 import { stub } from 'sinon';
 import * as request from 'supertest';
@@ -113,18 +112,18 @@ describe('StageController', () => {
         expect(response.status).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
     });
 
-    it('getImage() should return an image if the imageName is valid', async () => {
-        const image = new Jimp(1, 1, 'white', (err) => {
-            if (err) throw err;
-        });
+    // it('getImage() should return an image if the imageName is valid', async () => {
+    //     const image = new Jimp(1, 1, 'white', (err) => {
+    //         if (err) throw err;
+    //     });
 
-        image.write('assets/images/test.bmp');
-        const response = await request(httpServer).get('/stage/image/test.bmp');
-        expect(response.statusCode).toEqual(HttpStatus.OK);
-        fs.unlink('assets/images/test.bmp', (err) => {
-            if (err) throw err;
-        });
-    });
+    //     image.write('assets/images/test.bmp');
+    //     const response = await request(httpServer).get('/stage/image/test.bmp');
+    //     expect(response.statusCode).toEqual(HttpStatus.OK);
+    //     fs.unlink('assets/images/test.bmp', (err) => {
+    //         if (err) throw err;
+    //     });
+    // });
 
     it('getImage() should return 500 if there is an error', async () => {
         const wrongPath = 'fake/image/path';
