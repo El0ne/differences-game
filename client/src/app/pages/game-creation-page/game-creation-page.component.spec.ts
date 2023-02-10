9666631df73510d98542d32cbe80e53ea97a4877
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-10 09:35:40 -0500

    fixed ngModel error

--- client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { GameCreationPageComponent } from './game-creation-page.component';
 
@@ -11,7 +12,7 @@ describe('GameCreationPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [GameCreationPageComponent],
-            imports: [HttpClientModule],
+            imports: [HttpClientModule, FormsModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(GameCreationPageComponent);
