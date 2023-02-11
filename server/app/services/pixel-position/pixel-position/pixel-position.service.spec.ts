53d2f0a206dac6d3c96c032146b2bba206fd6f0a
Sam <sam.younan@polymtl.ca>
2023-02-11 18:37:10 -0500

    Added tests for the pixel position service

--- server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
+++ server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { ImageDimensionsService } from '@app/services/image-dimensions/image-dimensions.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PixelPositionService } from './pixel-position.service';
@@ -15,5 +16,15 @@ describe('PixelPositionService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('getXCoordinate should return a pixels x coordinate', () => {
+        const xCoordinate = service.getXCoordinate(999);
+        expect(xCoordinate).toEqual(359);
+    });
+
+    it('getYCoordinate should return a pixels y coordinate', () => {
+        const yCoordinate = service.getYCoordinate(9999);
+        expect(yCoordinate).toEqual(15);
     });
 });
