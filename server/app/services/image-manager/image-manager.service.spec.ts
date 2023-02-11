7aa9fb86fac5d2969cb72be36b335cfc63894b29
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-11 11:55:17 -0500

    tested file deletion function

--- server/app/services/image-manager/image-manager.service.spec.ts
+++ server/app/services/image-manager/image-manager.service.spec.ts
@@ -1,18 +1,29 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { Test, TestingModule } from '@nestjs/testing';
+import * as fs from 'fs';
 import { ImageManagerService } from './image-manager.service';
-
 describe('ImageManagerService', () => {
-  let service: ImageManagerService;
+    let service: ImageManagerService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ImageManagerService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [ImageManagerService],
+        }).compile();
 
-    service = module.get<ImageManagerService>(ImageManagerService);
-  });
+        service = module.get<ImageManagerService>(ImageManagerService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should delete the image at the provided path', () => {
+        const imagePath = 'assets/images/test.bmp';
+        fs.writeFileSync(imagePath, 'Test image data');
+        expect(fs.existsSync(imagePath)).toBe(true);
+        service.deleteImage(imagePath);
+        setTimeout(() => {
+            expect(fs.existsSync(imagePath)).toBe(false);
+        }, 100);
+    });
 });
--- server/app/services/image-manager/image-manager.service.ts
+++ server/app/services/image-manager/image-manager.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+
 @Injectable()
 export class ImageManagerService {
     deleteImage(imagePath: string) {
