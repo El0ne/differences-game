1f44794c723806ccb85517a1d15524607d73e22b
Chahine <tchamitouche@gmail.com>
2023-02-13 10:45:24 -0500

    using Alexios constants and cleaned some shit

--- client/src/app/pages/game-creation-page/game-creation-constants.ts
+++ client/src/app/pages/game-creation-page/game-creation-constants.ts
@@ -1,9 +1,3 @@
-export const IMAGE = {
-    width: 640,
-    height: 480,
-    size: 921654, // 640px * 480px * 3 bytes (24 bits).
-};
-
 export const GC_PATHS = {
     erase: '../../../assets/eraser.png',
     invert: './../../assets/invert-tool.png',
