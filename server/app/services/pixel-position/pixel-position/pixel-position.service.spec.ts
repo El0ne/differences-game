81c50e05c25e06d129ca0b42a8af5854932adfad
Sam <sam.younan@polymtl.ca>
2023-02-13 12:24:34 -0500

    Added out of bound testing with stubs

--- server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
+++ server/app/services/pixel-position/pixel-position/pixel-position.service.spec.ts
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import { ImageDimensionsService } from '@app/services/image-dimensions/image-dimensions.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { PixelPositionService } from './pixel-position.service';
+import { stub } from 'sinon';
+import { outOfBoundConstant, PixelPositionService } from './pixel-position.service';
 
 describe('PixelPositionService', () => {
     let service: PixelPositionService;
+    let imageDimensionsService: ImageDimensionsService;
+    let getWidthStub;
+    let getHeightStub;
+    let getNumberOfPixelsStub;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -12,6 +17,10 @@ describe('PixelPositionService', () => {
         }).compile();
 
         service = module.get<PixelPositionService>(PixelPositionService);
+        imageDimensionsService = module.get<ImageDimensionsService>(ImageDimensionsService);
+        getWidthStub = stub(imageDimensionsService, 'getWidth').returns(640);
+        getHeightStub = stub(imageDimensionsService, 'getHeight').returns(480);
+        getNumberOfPixelsStub = stub(imageDimensionsService, 'getNumberOfPixels').returns(307200);
     });
 
     it('should be defined', () => {
@@ -19,12 +28,32 @@ describe('PixelPositionService', () => {
     });
 
     it('getXCoordinate should return a pixels x coordinate', () => {
-        const xCoordinate = service.getXCoordinate(999);
-        expect(xCoordinate).toEqual(359);
+        const xCoordinate = service.getXCoordinate(640);
+        expect(xCoordinate).toEqual(0);
     });
 
     it('getYCoordinate should return a pixels y coordinate', () => {
-        const yCoordinate = service.getYCoordinate(9999);
-        expect(yCoordinate).toEqual(15);
+        const yCoordinate = service.getYCoordinate((640 - 1) * 480);
+        expect(yCoordinate).toEqual(480 - 1);
+    });
+
+    it('getXCoordinate should not return a pixels x coordinate if pixel position is negative', () => {
+        const xCoordinate = service.getXCoordinate(-getWidthStub);
+        expect(xCoordinate).toEqual(outOfBoundConstant);
+    });
+
+    it('getYCoordinate should not return a pixels y coordinate if pixel position is negative', () => {
+        const yCoordinate = service.getYCoordinate(-getHeightStub);
+        expect(yCoordinate).toEqual(outOfBoundConstant);
+    });
+
+    it('getXCoordinate should not return a pixels x coordinate if pixel position is greater than the array length', () => {
+        const xCoordinate = service.getXCoordinate(getNumberOfPixelsStub);
+        expect(xCoordinate).toEqual(outOfBoundConstant);
+    });
+
+    it('getYCoordinate should not return a pixels y coordinate if pixel position is greater than the array length', () => {
+        const yCoordinate = service.getYCoordinate(getNumberOfPixelsStub);
+        expect(yCoordinate).toEqual(outOfBoundConstant);
     });
 });
--- server/app/services/pixel-position/pixel-position/pixel-position.service.ts
+++ server/app/services/pixel-position/pixel-position/pixel-position.service.ts
@@ -1,14 +1,22 @@
 import { ImageDimensionsService } from '@app/services/image-dimensions/image-dimensions.service';
 import { Injectable } from '@nestjs/common';
 
+export const outOfBoundConstant = 0;
+
 @Injectable()
 export class PixelPositionService {
     constructor(private imageDimensionsService: ImageDimensionsService) {}
     getXCoordinate(pixelNumber: number): number {
-        return pixelNumber % this.imageDimensionsService.getWidth();
+        if (pixelNumber >= 0 && pixelNumber < this.imageDimensionsService.getNumberOfPixels()) {
+            return pixelNumber % this.imageDimensionsService.getWidth();
+        }
+        return outOfBoundConstant;
     }
 
     getYCoordinate(pixelNumber: number): number {
-        return Math.floor(pixelNumber / this.imageDimensionsService.getWidth());
+        if (pixelNumber >= 0 && pixelNumber < this.imageDimensionsService.getNumberOfPixels()) {
+            return Math.floor(pixelNumber / this.imageDimensionsService.getWidth());
+        }
+        return outOfBoundConstant;
     }
 }
