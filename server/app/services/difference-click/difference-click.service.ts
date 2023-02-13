6d90b350937d23e94a9ba6acb1cc949c74e715e9
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-13 11:47:55 -0500

    added methods to add differences in json

--- server/app/services/difference-click/difference-click.service.ts
+++ server/app/services/difference-click/difference-click.service.ts
@@ -1,15 +1,30 @@
+import { DifferencesCounterService } from '@app/services/differences-counter/differences-counter.service';
 import { ClickDifferenceVerification } from '@common/click-difference-verification';
+import { DifferencesObject } from '@common/differences-object';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-import { DifferencesCounterService } from '../differences-counter/differences-counter.service';
 import { WIDTH } from './image-constants';
-
 @Injectable()
 export class DifferenceClickService {
     jsonPath = path.join(process.cwd(), 'app/dataBase/differences.json');
     content = fs.readFileSync(this.jsonPath, 'utf8');
     constructor(private differenceCounterService: DifferencesCounterService) {}
+
+    getAllDifferenceArrays(): DifferencesObject[] {
+        const content = fs.readFileSync(this.jsonPath, 'utf8');
+        return JSON.parse(content).differenceObjects;
+    }
+    createDifferenceArray(gameId: string, differencesArray: number[][]): DifferencesObject {
+        const allDifferenceArrays = this.getAllDifferenceArrays();
+        const newDifferenceArray: DifferencesObject = {
+            id: gameId,
+            differences: differencesArray,
+        };
+        allDifferenceArrays.push(newDifferenceArray);
+        fs.writeFileSync(this.jsonPath, JSON.stringify({ differenceObjects: allDifferenceArrays }));
+        return newDifferenceArray;
+    }
 
     getDifferenceArrayFromStageID(stageId: string): number[][] {
         for (const differenceObject of JSON.parse(this.content).differences) {
