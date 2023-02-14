76eaa1811fdc4c119193e3981dc9627d6caf1969
JulienLegare <julien.legare@polymtl.ca>
2023-02-14 15:55:59 -0500

    deleted comments

--- client/src/app/services/click-event/click-event.service.spec.ts
+++ client/src/app/services/click-event/click-event.service.spec.ts
@@ -4,23 +4,13 @@ import { ClickEventService } from '@app/services/click-event/click-event.service
 
 describe('ClickEventService', () => {
     let service: ClickEventService;
-    // let httpController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
         service = TestBed.inject(ClickEventService);
-        // httpController = TestBed.inject(HttpTestingController);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-
-    /*
-    it('should make a GET request to the correct URL with the correct parameters', () => {
-        service.isADifference(0, 0, '').subscribe((res) => {
-            expect(res).toBeTruthy();
-        });
-    });
-    */
 });
