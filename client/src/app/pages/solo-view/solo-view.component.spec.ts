0fa39313793e75c0ece6dbbfb882d510e4f7626a
Jasper31 <jasperlai4@gmail.com>
2023-02-26 00:09:11 -0500

    Messages modification

--- client/src/app/pages/solo-view/solo-view.component.spec.ts
+++ client/src/app/pages/solo-view/solo-view.component.spec.ts
@@ -3,10 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ClickEventComponent } from '@app/components/click-event/click-event.component';
 import { ChosePlayerNameDialogComponent } from '@app/modals/chose-player-name-dialog/chose-player-name-dialog.component';
 import { ClickEventService } from '@app/services/click-event/click-event.service';
+import { GameCardInformationService } from '@app/services/game-card-information-service/game-card-information.service';
 import { GameCardInformation } from '@common/game-card';
 import { of } from 'rxjs/internal/observable/of';
 import { MESSAGES_LENGTH } from './solo-view-constants';
@@ -17,8 +19,15 @@ describe('SoloViewComponent', () => {
     let fixture: ComponentFixture<SoloViewComponent>;
     let modalSpy: MatDialog;
     let afterClosedSpy: MatDialogRef<ChosePlayerNameDialogComponent>;
+    let mockService: GameCardInformationService;
+    const mockActivatedRoute = { snapshot: { paramMap: { get: () => '234' } } };
+    const mockRouter = { url: '1v1/234' };
 
     beforeEach(async () => {
+        mockService = jasmine.createSpyObj('GameCardInformationService', ['getGameCardInfoFromId']);
+        mockService.getGameCardInfoFromId = () => {
+            return of(SERVICE_MOCK_GAME_CARD);
+        };
         modalSpy = jasmine.createSpyObj('MatDialog', ['open']);
         afterClosedSpy = jasmine.createSpyObj('MatDialogRef<ChosePlayerNameDialogComponent>', ['afterClosed']);
         afterClosedSpy.afterClosed = () => of('test');
@@ -27,7 +36,13 @@ describe('SoloViewComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [SoloViewComponent, ClickEventComponent, ChosePlayerNameDialogComponent],
             imports: [FormsModule, HttpClientTestingModule, RouterTestingModule, MatIconModule, MatDialogModule],
-            providers: [{ provide: ClickEventService }, { provide: MatDialog, useValue: modalSpy }],
+            providers: [
+                { provide: ClickEventService },
+                { provide: MatDialog, useValue: modalSpy },
+                { provide: ActivatedRoute, useValue: mockActivatedRoute },
+                { provide: Router, useValue: mockRouter },
+                { provide: GameCardInformationService, useValue: mockService },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SoloViewComponent);
@@ -38,6 +53,14 @@ describe('SoloViewComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set the current gameCard id according to value in route and request gameCard', () => {
+        component.ngOnInit();
+        expect(component.currentGameId).toEqual('234');
+        expect(component.is1v1).toBe(true);
+        expect(component.gameCardInfo).toBe(SERVICE_MOCK_GAME_CARD);
+        expect(component.numberOfDifferences).toEqual(SERVICE_MOCK_GAME_CARD.differenceNumber);
     });
 
     it('showTextBox attribute should turn to true when toggleInfoCard is called and showTextBox is false', () => {
@@ -104,16 +127,16 @@ describe('SoloViewComponent', () => {
     });
 
     it('should increment counter when increment counter is called', () => {
-        component.currentScore = 0;
+        component.currentScorePlayer1 = 0;
         component.incrementScore();
         const answer = 1;
 
-        expect(component.currentScore).toEqual(answer);
+        expect(component.currentScorePlayer1).toEqual(answer);
     });
 
     it('finishGame should have been called if number of errors is equal to the current score in incrementScore', () => {
         const finishGameSpy = spyOn(component, 'finishGame');
-        component.currentScore = 1;
+        component.currentScorePlayer1 = 1;
         component.numberOfDifferences = 2;
         component.incrementScore();
         expect(finishGameSpy).toHaveBeenCalled();
@@ -163,6 +186,25 @@ const FAKE_GAME_CARD: GameCardInformation = {
     name: 'game.name',
     difficulty: 'Facile',
     differenceNumber: 6,
+    originalImageName: 'game.baseImage',
+    differenceImageName: 'game.differenceImage',
+    soloTimes: [
+        { time: 0, name: '--' },
+        { time: 0, name: '--' },
+        { time: 0, name: '--' },
+    ],
+    multiTimes: [
+        { time: 0, name: '--' },
+        { time: 0, name: '--' },
+        { time: 0, name: '--' },
+    ],
+};
+
+const SERVICE_MOCK_GAME_CARD: GameCardInformation = {
+    id: '0',
+    name: 'game',
+    difficulty: 'Facile',
+    differenceNumber: 7,
     originalImageName: 'game.baseImage',
     differenceImageName: 'game.differenceImage',
     soloTimes: [
