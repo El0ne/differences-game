3dadfdb02a1f557a5f4ff3170eddf1fdc8406732
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-02-28 21:35:39 -0500

    commented all game cards module and moved it in app module directly

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -3,11 +3,17 @@ import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GameCardModule } from 'game-card/schemas/game-card.module';
+import { GameCard, gameCardSchema } from 'game-card/schemas/game-cards.schemas';
 import { GameClickController } from './controllers/game-click/game-click.controller';
+import { StageController } from './controllers/stage/stage.controller';
 import { DataBaseService } from './services/data-base/data-base.service';
 import { DifferenceClickService } from './services/difference-click/difference-click.service';
+import { DifferenceDetectionService } from './services/difference-detection/difference-detection.service';
 import { DifferencesCounterService } from './services/differences-counter/differences-counter.service';
+import { GameCardService } from './services/game-card/game-card.service';
+import { GameDifficultyService } from './services/game-difficulty/game-difficulty.service';
 import { ImageDimensionsService } from './services/image-dimensions/image-dimensions.service';
+import { ImageManagerService } from './services/image-manager/image-manager.service';
 import { PixelPositionService } from './services/pixel-position/pixel-position/pixel-position.service';
 import { PixelRadiusService } from './services/pixel-radius/pixel-radius.service';
 
@@ -21,19 +27,23 @@ import { PixelRadiusService } from './services/pixel-radius/pixel-radius.service
                 uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
             }),
         }),
-        MongooseModule.forFeature([]),
+        MongooseModule.forFeature([{ name: GameCard.name, schema: gameCardSchema }]),
         GameCardModule,
     ],
-    controllers: [GameClickController],
+    controllers: [GameClickController, StageController],
     providers: [
         ChatGateway,
         Logger,
+        GameCardService,
+        GameDifficultyService,
         DifferenceClickService,
+        DifferenceDetectionService,
         DataBaseService,
         DifferencesCounterService,
         PixelRadiusService,
         PixelPositionService,
         ImageDimensionsService,
+        ImageManagerService,
     ],
 })
 export class AppModule {}
