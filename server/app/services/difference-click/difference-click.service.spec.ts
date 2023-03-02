7797bdbfe1ec73131e164cdcc6894b87c006d98f
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-02 12:11:12 -0500

    first test and suite config seems good

--- server/app/services/difference-click/difference-click.service.spec.ts
+++ server/app/services/difference-click/difference-click.service.spec.ts
@@ -1,78 +1,114 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
+import { Differences, DifferencesDocument, differencesSchema } from '@app/schemas/differences.schemas';
 import { DifferencesCounterService } from '@app/services/differences-counter/differences-counter.service';
 import { ImageDimensionsService } from '@app/services/image-dimensions/image-dimensions.service';
 import { PixelPositionService } from '@app/services/pixel-position/pixel-position/pixel-position.service';
 import { PixelRadiusService } from '@app/services/pixel-radius/pixel-radius.service';
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as path from 'path';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Model } from 'mongoose';
 import { DifferenceClickService } from './difference-click.service';
-import { differenceObjects } from './differents-test.json';
 
 describe('DifferenceClickService', () => {
     let service: DifferenceClickService;
     let imageDimService: ImageDimensionsService;
     let differenceCounterService: DifferencesCounterService;
+    let differenceModel: Model<DifferencesDocument>;
+    let mongoServer: MongoMemoryServer;
 
     beforeEach(async () => {
+        mongoServer = await MongoMemoryServer.create();
+
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                MongooseModule.forRootAsync({
+                    useFactory: () => ({
+                        uri: mongoServer.getUri(),
+                    }),
+                }),
+                MongooseModule.forFeature([{ name: Differences.name, schema: differencesSchema }]),
+            ],
             providers: [DifferenceClickService, DifferencesCounterService, PixelRadiusService, PixelPositionService, ImageDimensionsService],
         }).compile();
 
         service = module.get<DifferenceClickService>(DifferenceClickService);
-        service.jsonPath = path.join(__dirname, '/differents-test.json');
+        // service.jsonPath = path.join(__dirname, '/differents-test.json');
 
         imageDimService = module.get<ImageDimensionsService>(ImageDimensionsService);
         differenceCounterService = module.get<DifferencesCounterService>(DifferencesCounterService);
+        differenceModel = module.get<Model<DifferencesDocument>>(getModelToken(Differences.name));
+        await differenceModel.deleteMany({});
+    });
+
+    const DELAY_BEFORE_CLOSING_CONNECTION = 200;
+
+    afterEach((done) => {
+        setTimeout(async () => {
+            await mongoServer.stop();
+            done();
+        }, DELAY_BEFORE_CLOSING_CONNECTION);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+        expect(differenceModel).toBeDefined();
     });
 
     it('getAlldifferenceArrays should return all game differences from arrays', async () => {
-        const differences = service.getAllDifferenceArrays();
-        expect(differences).toEqual(differenceObjects);
+        expect((await service.getAllDifferenceArrays()).length).toEqual(0);
+        const differenceObject = getFakeDifferences();
+        await differenceModel.create(differenceObject);
+        expect((await service.getAllDifferenceArrays()).length).toEqual(1);
+        expect(await service.getAllDifferenceArrays()).toEqual([expect.objectContaining(differenceObject)]);
     });
 
-    it('getDifferenceArrayFromStageId should return an array given a certain id', async () => {
-        const differences = service.getDifferenceArrayFromStageID('1');
-        expect(differences).toEqual(differenceObjects.find((difference) => (difference.id = '1')).differences);
-    });
+    // it('getDifferenceArrayFromStageId should return an array given a certain id', async () => {
+    //     const differences = service.getDifferenceArrayFromStageID('1');
+    //     expect(differences).toEqual(differenceObjects.find((difference) => (difference.id = '1')).differences);
+    // });
 
-    it('getDifferenceArrayFromStageId should return an empty array if id is not found', async () => {
-        const differences = service.getDifferenceArrayFromStageID('1000');
-        expect(differences).toEqual([]);
-    });
+    // it('getDifferenceArrayFromStageId should return an empty array if id is not found', async () => {
+    //     const differences = service.getDifferenceArrayFromStageID('1000');
+    //     expect(differences).toEqual([]);
+    // });
 
-    it('createDifferenceArray should create a difference array given an id and the array', async () => {
-        const differences = service.getAllDifferenceArrays();
-        service.createDifferenceArray('2', [
-            [123, 124, 125],
-            [1, 2, 3],
-        ]);
-        const addedDifferences = service.getAllDifferenceArrays();
-        const getDifferences = service.getDifferenceArrayFromStageID('2');
-        expect(differences.length).not.toEqual(addedDifferences.length);
-        expect(getDifferences).toBeTruthy();
-    });
+    // it('createDifferenceArray should create a difference array given an id and the array', async () => {
+    //     const differences = service.getAllDifferenceArrays();
+    //     service.createDifferenceArray('2', [
+    //         [123, 124, 125],
+    //         [1, 2, 3],
+    //     ]);
+    //     const addedDifferences = service.getAllDifferenceArrays();
+    //     const getDifferences = service.getDifferenceArrayFromStageID('2');
+    //     expect(differences.length).not.toEqual(addedDifferences.length);
+    //     expect(getDifferences).toBeTruthy();
+    // });
 
-    it('validateDifferencePosition should determine if click is a difference', async () => {
-        jest.spyOn(imageDimService, 'getWidth').mockReturnValue(640);
-        jest.spyOn(differenceCounterService, 'findPixelDifferenceIndex').mockReturnValue(0);
+    // it('validateDifferencePosition should determine if click is a difference', async () => {
+    //     jest.spyOn(imageDimService, 'getWidth').mockReturnValue(640);
+    //     jest.spyOn(differenceCounterService, 'findPixelDifferenceIndex').mockReturnValue(0);
 
-        const result = service.validateDifferencePositions(0, 0, '1');
-        expect(result.isADifference).toBe(true);
-        expect(result.differenceArray).toEqual([0, 1, 2, 3]);
-        expect(result.differencesPosition).toEqual(0);
-    });
+    //     const result = service.validateDifferencePositions(0, 0, '1');
+    //     expect(result.isADifference).toBe(true);
+    //     expect(result.differenceArray).toEqual([0, 1, 2, 3]);
+    //     expect(result.differencesPosition).toEqual(0);
+    // });
 
-    it('validateDifferencePosition should determine if click is not a difference', async () => {
-        jest.spyOn(imageDimService, 'getWidth').mockReturnValue(640);
-        jest.spyOn(differenceCounterService, 'findPixelDifferenceIndex').mockReturnValue(2);
+    // it('validateDifferencePosition should determine if click is not a difference', async () => {
+    //     jest.spyOn(imageDimService, 'getWidth').mockReturnValue(640);
+    //     jest.spyOn(differenceCounterService, 'findPixelDifferenceIndex').mockReturnValue(2);
 
-        const result = service.validateDifferencePositions(100, 100, '1');
-        expect(result.isADifference).toBe(false);
-        expect(result.differencesPosition).toEqual(2);
-        expect(result.differenceArray).toEqual([]);
-    });
+    //     const result = service.validateDifferencePositions(100, 100, '1');
+    //     expect(result.isADifference).toBe(false);
+    //     expect(result.differencesPosition).toEqual(2);
+    //     expect(result.differenceArray).toEqual([]);
+    // });
+});
+
+const getFakeDifferences = (): Differences => ({
+    differences: [
+        [0, 1, 2, 3],
+        [7, 8, 9, 10],
+    ],
 });
