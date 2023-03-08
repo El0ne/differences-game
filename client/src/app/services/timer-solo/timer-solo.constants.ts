2886147cac154de3ce531d2ec8517bc228fcde69
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-08 16:02:40 -0500

    init timer at 0

--- client/src/app/services/timer-solo/timer-solo.constants.ts
+++ client/src/app/services/timer-solo/timer-solo.constants.ts
@@ -1,5 +1,4 @@
 export const ZERO = 0;
 export const ONE_SEC_IN_MS = 1000;
-export const START_TIME = -1;
 export const TEN_SECONDS = 10;
 export const TEN_SEC_IN_MS = 10000;
--- client/src/app/services/timer-solo/timer-solo.service.ts
+++ client/src/app/services/timer-solo/timer-solo.service.ts
@@ -2,34 +2,19 @@ import { Injectable } from '@angular/core';
 import { SocketService } from '@app/services/socket/socket.service';
 import { ChatEvents } from '@common/chat.gateway.events';
 import { Subscription } from 'rxjs';
-import { START_TIME } from './timer-solo.constants';
 
 @Injectable({
     providedIn: 'root',
 })
 export class TimerSoloService {
-    currentTime: number = START_TIME;
+    currentTime: number = 0;
     subArray: Subscription[] = [];
 
     constructor(private chat: SocketService) {}
 
     startTimer(): void {
-        // console.log('coneected');
-        this.currentTime = -1;
-        // const timer1 = timer(ZERO, ONE_SEC_IN_MS);
-
-        // const sub = timer1.subscribe(() => {
-        //     this.currentTime++;
-        //     console.log('this.currentTime', this.currentTime);
-        // });
-
-        // this.subArray.push(sub);
-
-        // this.chat.connect();
         this.chat.send('start timer', this.chat.gameRoom);
-
         this.chat.listen<number>(ChatEvents.Elouan, (timerValue: number) => {
-            console.log('timerValue', timerValue);
             this.currentTime = timerValue;
         });
     }
