31965e68cf0690890e0ea20aaa674f07b9da39c4
Alexis Nicolas <alexisducanada@hotmail.com>
2023-03-10 13:30:57 -0500

    tested delete method of Socket

--- client/src/app/services/socket/socket.service.spec.ts
+++ client/src/app/services/socket/socket.service.spec.ts
@@ -2,15 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { SocketService } from './socket.service';
 
-describe('ChatSocketService', () => {
+describe('SocketService', () => {
     let service: SocketService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(SocketService);
+        service.sio = jasmine.createSpyObj('Socket', ['on', 'off', 'emit', 'disconnect']);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('delete should delete the listener of the sio', () => {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        service.connect();
+        service.sio.on('testEvent', () => {});
+        service.delete('testEvent');
+        expect(service.sio.hasListeners('testEvent')).toBeFalsy();
     });
 });
