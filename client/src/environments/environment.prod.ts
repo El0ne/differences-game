027eb0d1bb4d0e3ca2676ab519c4771c0fd0bcbb
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-12 15:29:50 -0400

    added var for socket in prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     serverUrl: 'http://ec2-35-183-130-205.ca-central-1.compute.amazonaws.com:3000/api',
+    serverSocket: 'http://ec2-35-183-130-205.ca-central-1.compute.amazonaws.com:3000',
 };
