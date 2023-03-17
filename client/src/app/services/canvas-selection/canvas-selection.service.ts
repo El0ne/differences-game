cdad2c6bdd0bec158af7e44802fd53f2d4e70c1a
Chahine <tchamitouche@gmail.com>
2023-03-17 17:00:50 -0400

    started to implement a few services

--- client/src/app/services/canvas-selection/canvas-selection.service.ts
+++ client/src/app/services/canvas-selection/canvas-selection.service.ts
@@ -11,6 +11,28 @@ export class CanvasSelectionService {
         this.canvasInformations = information;
     }
 
+    pushCanvas(canvas: HTMLCanvasElement) {
+        this.canvasInformations.nbElements++;
+        // if (this.nbElements < this.actionsArray.length) {
+        //     // added this to make sure that if someone undos then adds new modifications
+        //     // that it won't allow user to undo back to the old saved canvases
+        //     this.actionsArray.length = this.nbElements;
+        // }
+
+        const canvasDataURL = canvas.toDataURL();
+        if (this.canvasInformations.isInOriginalCanvas) {
+            // if (this.isFirstTimeInLeftCanvas) this.verifyFirstTime('left');
+            this.canvasInformations.actionsArray.push(true);
+            this.canvasInformations.leftArrayPointer++;
+            this.canvasInformations.leftCanvasArray.push(canvasDataURL);
+        } else {
+            this.canvasInformations.actionsArray.push(false);
+            this.canvasInformations.rightArrayPointer++;
+            this.canvasInformations.rightCanvasArray.push(canvasDataURL);
+            // if (this.isFirstTimeInRightCanvas) this.verifyFirstTime('right');
+        }
+    }
+
     choseCanvas(mouseEvent: MouseEvent): void {
         if ([this.canvasInformations.differenceRectangleCanvas, this.canvasInformations.differenceDrawnCanvas].includes(mouseEvent.target)) {
             this.canvasInformations.isInOriginalCanvas = false;
--- client/src/app/services/drawing-rectangle/drawing-rectangle.service.ts
+++ client/src/app/services/drawing-rectangle/drawing-rectangle.service.ts
@@ -14,55 +14,55 @@ export class DrawingRectangleService {
     }
 
     startDrawingRectangle(mouseEvent: MouseEvent): void {
-        const canvas = this.drawingCanvas2.getBoundingClientRect();
-        this.isUserClicking = true;
+        const canvas = this.canvasInformations.drawingCanvas2.getBoundingClientRect();
+        this.canvasInformations.isUserClicking = true;
 
-        this.rectangleInitialX = mouseEvent.clientX - canvas.left;
-        this.rectangleInitialY = mouseEvent.clientY - canvas.top;
+        this.canvasInformations.rectangleInitialX = mouseEvent.clientX - canvas.left;
+        this.canvasInformations.rectangleInitialY = mouseEvent.clientY - canvas.top;
     }
 
     stopDrawingRectangle(): void {
-        const firstContext = this.drawingCanvas1.getContext('2d');
-        const secondContext = this.drawingCanvas2.getContext('2d');
-        this.isUserClicking = false;
+        const firstContext = this.canvasInformations.drawingCanvas1.getContext('2d');
+        const secondContext = this.canvasInformations.drawingCanvas2.getContext('2d');
+        this.canvasInformations.isUserClicking = false;
 
-        if (firstContext) firstContext.drawImage(this.drawingCanvas2, 0, 0);
-        if (secondContext) secondContext.clearRect(0, 0, this.drawingCanvas2.width, this.drawingCanvas2.height);
+        if (firstContext) firstContext.drawImage(this.canvasInformations.drawingCanvas2, 0, 0);
+        if (secondContext) secondContext.clearRect(0, 0, this.canvasInformations.drawingCanvas2.width, this.canvasInformations.drawingCanvas2.height);
 
-        this.pushCanvas(this.drawingCanvas1);
+        this.pushCanvas(this.canvasInformations.drawingCanvas1);
     }
 
     paintRectangle(mouseEvent: MouseEvent): void {
         this.canvasSelection.choseCanvas(mouseEvent);
-        const context = this.drawingCanvas2.getContext('2d');
+        const context = this.canvasInformations.drawingCanvas2.getContext('2d');
 
-        if (context && this.isUserClicking) {
-            const canvas = this.drawingCanvas2.getBoundingClientRect();
-            context.fillStyle = this.selectedColor;
-            context.clearRect(0, 0, this.drawingCanvas2.width, this.drawingCanvas2.height);
+        if (context && this.canvasInformations.isUserClicking) {
+            const canvas = this.canvasInformations.drawingCanvas2.getBoundingClientRect();
+            context.fillStyle = this.canvasInformations.selectedColor;
+            context.clearRect(0, 0, this.canvasInformations.drawingCanvas2.width, this.drawingCanvas2.height);
 
-            const width = mouseEvent.clientX - canvas.left - this.rectangleInitialX;
-            const height = mouseEvent.clientY - canvas.top - this.rectangleInitialY;
+            const width = mouseEvent.clientX - canvas.left - this.canvasInformations.rectangleInitialX;
+            const height = mouseEvent.clientY - canvas.top - this.canvasInformations.rectangleInitialY;
 
             if (mouseEvent.shiftKey) {
                 const size = Math.min(Math.abs(width), Math.abs(height));
                 const signX = Math.sign(width);
                 const signY = Math.sign(height);
-                context.fillRect(this.rectangleInitialX, this.rectangleInitialY, size * signX, size * signY);
+                context.fillRect(this.canvasInformations.rectangleInitialX, this.canvasInformations.rectangleInitialY, size * signX, size * signY);
             } else {
-                context.fillRect(this.rectangleInitialX, this.rectangleInitialY, width, height);
+                context.fillRect(this.canvasInformations.rectangleInitialX, this.canvasInformations.rectangleInitialY, width, height);
             }
         }
     }
 
     drawRectangle(): void {
-        this.originalRectangleCanvas.nativeElement.addEventListener('mousedown', this.rectangleListener[0]);
-        this.differenceRectangleCanvas.nativeElement.addEventListener('mousedown', this.rectangleListener[0]);
+        this.canvasInformations.originalRectangleCanvas.nativeElement.addEventListener('mousedown', this.canvasInformations.rectangleListener[0]);
+        this.canvasInformations.differenceRectangleCanvas.nativeElement.addEventListener('mousedown', this.canvasInformations.rectangleListener[0]);
 
-        this.originalRectangleCanvas.nativeElement.addEventListener('mouseup', this.rectangleListener[1]);
-        this.differenceRectangleCanvas.nativeElement.addEventListener('mouseup', this.rectangleListener[1]);
+        this.canvasInformations.originalRectangleCanvas.nativeElement.addEventListener('mouseup', this.canvasInformations.rectangleListener[1]);
+        this.canvasInformations.differenceRectangleCanvas.nativeElement.addEventListener('mouseup', this.canvasInformations.rectangleListener[1]);
 
-        this.originalRectangleCanvas.nativeElement.addEventListener('mousemove', this.rectangleListener[2]);
-        this.differenceRectangleCanvas.nativeElement.addEventListener('mousemove', this.rectangleListener[2]);
+        this.canvasInformations.originalRectangleCanvas.nativeElement.addEventListener('mousemove', this.canvasInformations.rectangleListener[2]);
+        this.canvasInformations.differenceRectangleCanvas.nativeElement.addEventListener('mousemove', this.canvasInformations.rectangleListener[2]);
     }
 }
