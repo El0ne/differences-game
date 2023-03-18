51f5da11bc0b451f1373015e711756cf7ac956ab
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-18 18:43:07 -0400

    fixed imports for match gateway

--- server/app/gateways/match/match/match.gateway.spec.ts
+++ server/app/gateways/match/match/match.gateway.spec.ts
@@ -1,12 +1,14 @@
+import { GameManagerService } from '@app/services/game-manager/game-manager.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchGateway } from './match.gateway';
 
 describe('MatchGateway', () => {
     let gateway: MatchGateway;
+    const mockService = { endGame: jest.fn() };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [MatchGateway],
+            providers: [MatchGateway, { provide: GameManagerService, useValue: mockService }],
         }).compile();
 
         gateway = module.get<MatchGateway>(MatchGateway);
--- server/app/gateways/match/match/match.gateway.ts
+++ server/app/gateways/match/match/match.gateway.ts
@@ -1,13 +1,12 @@
 import { GameManagerService } from '@app/services/game-manager/game-manager.service';
 import { Injectable } from '@nestjs/common';
-import { Logger } from '@nestjs/common/services/logger.service';
 import { OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 @Injectable()
 export class MatchGateway implements OnGatewayDisconnect {
-    constructor(private gameManagerService: GameManagerService, private logger: Logger) {}
+    constructor(private gameManagerService: GameManagerService) {}
 
     handleDisconnect(socket: Socket): void {
         if (socket.data.stageId) {
