8f37660381e303f875746ca877a8c8d76f2b3e1f
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-19 19:22:03 -0400

    finished tests for draw manipulation

--- client/src/app/services/draw-manipulation/draw-manipulation.service.spec.ts
+++ client/src/app/services/draw-manipulation/draw-manipulation.service.spec.ts
@@ -20,6 +20,31 @@ describe('DrawManipulationService', () => {
         service.setProperties(fakeCanvasInfo);
         expect(service.canvasInformations).toEqual(fakeCanvasInfo);
     });
+
+    it('should invert the canvas', () => {
+        // Draw something on the canvas before inverting
+        const fakeCanvasInfo = getFakeCanvasInformations();
+        service.setProperties(fakeCanvasInfo);
+        const size = 10;
+        const originalDrawingContext = service.canvasInformations.originalDrawnCanvas.getContext('2d');
+        originalDrawingContext!.fillStyle = 'ffffff';
+        originalDrawingContext!.fillRect(0, 0, size, size);
+
+        const differenceDrawingContext = service.canvasInformations.differenceDrawnCanvas.getContext('2d');
+        differenceDrawingContext!.fillStyle = '000000';
+        differenceDrawingContext!.fillRect(0, 0, size, size);
+
+        const originalData = originalDrawingContext!.getImageData(0, 0, size, size);
+        const differenceData = originalDrawingContext!.getImageData(0, 0, size, size);
+
+        service.invert();
+
+        const originalDrawingImageData = originalDrawingContext!.getImageData(0, 0, size, size);
+        const differenceDrawingImageData = differenceDrawingContext!.getImageData(0, 0, size, size);
+
+        expect(originalDrawingImageData.data).toEqual(differenceData!.data);
+        expect(differenceDrawingImageData.data).toEqual(originalData!.data);
+    });
     it('duplicate should call drawImage with differenceDrawing if it is defined and side is right', () => {
         const differenceCanvasMock = document.createElement('canvas');
         spyOn(differenceCanvasMock.getContext('2d')!, 'drawImage');
