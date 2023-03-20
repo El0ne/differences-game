38ed437ac4f5375a5e5f41bf169b3f5305289f29
JulienLegare <julien.legare@polymtl.ca>
2023-03-20 11:41:32 -0400

    fix for cheat mode 2.0

--- client/src/app/components/click-event/click-event-constant.ts
+++ client/src/app/components/click-event/click-event-constant.ts
@@ -2,4 +2,4 @@ export const WIDTH = 640;
 export const HEIGHT = 480;
 export const WAIT_TIME_MS = 1000;
 export const FAST_WAIT_TIME_MS = 100;
-export const MAX_EFFECT_TIME = 500;
+export const MAX_EFFECT_TIME = 600;
