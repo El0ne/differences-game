6ed6c9b600cfececf2152c50e10d1adf18e4a17c
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-25 15:05:24 -0400

    added status code

--- server/app/controllers/game-constants/game-constants.controller.ts
+++ server/app/controllers/game-constants/game-constants.controller.ts
@@ -1,18 +1,28 @@
 import { GameConstantService } from '@app/services/game-constant/game-constant.service';
 import { GameConstants } from '@common/game-constants';
-import { Body, Controller, Get, Put } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Put, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('game-constants')
 export class GameConstantsController {
     constructor(private gameConstantService: GameConstantService) {}
 
     @Get('/')
-    getGameConstants(): GameConstants {
-        return this.gameConstantService.getGameConstants();
+    getGameConstants(@Res() res: Response): void {
+        try {
+            res.status(HttpStatus.OK).send(this.gameConstantService.getGameConstants());
+        } catch {
+            throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Put('/')
-    updateGameConstants(@Body() gameConstants: GameConstants): void {
-        this.gameConstantService.updateGameConstants(gameConstants);
+    updateGameConstants(@Body() gameConstants: GameConstants, @Res() res: Response): void {
+        try {
+            this.gameConstantService.updateGameConstants(gameConstants);
+            res.status(HttpStatus.NO_CONTENT);
+        } catch {
+            throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
