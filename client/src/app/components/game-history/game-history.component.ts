3397af8f008519d245837c85c4534f6f3f760603
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-27 19:22:52 -0400

    added scrollbar

--- client/src/app/components/game-history/game-history.component.ts
+++ client/src/app/components/game-history/game-history.component.ts
@@ -80,7 +80,80 @@ export class GameHistoryComponent implements OnInit {
             isMultiplayer: true,
             isAbandon: false,
         },
+        {
+            id: 'gameID',
+            winnerName: 'jo',
+            player1Name: 'jo',
+            player2Name: 'seph',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: false,
+        },
+        {
+            id: 'gameID',
+            winnerName: 'seph',
+            player1Name: 'jo',
+            player2Name: 'seph',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: false,
+        },
+        {
+            id: 'gameID',
+            winnerName: 'seph',
+            player1Name: 'jo',
+            player2Name: 'seph',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: true,
+        },
+        {
+            id: 'gameID',
+            winnerName: 'jo',
+            player1Name: 'jo',
+            player2Name: 'seph',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: true,
+        },
+        {
+            id: 'gameID',
+            winnerName: '',
+            player1Name: 'jo',
+            player2Name: '',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: true,
+        },
+        {
+            id: 'gameID',
+            winnerName: 'jo',
+            player1Name: 'jo',
+            player2Name: '',
+            gameName: 'game-name',
+            gameMode: 'game-mode',
+            gameDuration: 170, // timerservice convert
+            startTime: '27/03/2023 13:09:07',
+            isMultiplayer: true,
+            isAbandon: false,
+        },
     ];
+
     dataSource = this.gameHistory;
     displayedColumns: string[] = ['date', 'duration', 'mode', 'player1', 'player2'];
 
