9fc6ed31f872cc246e1e4911d2a8424d1e9358c9
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-03-28 21:04:02 -0400

    uncommented test cause it now works

--- server/app/controllers/game-constants/game-constants.controller.spec.ts
+++ server/app/controllers/game-constants/game-constants.controller.spec.ts
@@ -50,12 +50,11 @@ describe('GameConstantsController', () => {
         expect(response.status).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
     });
 
-    // TODO fix test
-    // it('updateGameConstants() should call GameConstantService.updateGameConstants() with the body as a parameter', async () => {
-    //     const response = await request(httpServer).put('/game-constants').send(FAKE_GAME_CONSTANTS);
-    //     assert(updateGameConstantsStub.called);
-    //     expect(response.status).toBe(HttpStatus.NO_CONTENT);
-    // });
+    it('updateGameConstants() should call GameConstantService.updateGameConstants() with the body as a parameter', async () => {
+        const response = await request(httpServer).put('/game-constants').send(DEFAULT_GAME_CONSTANTS);
+        assert(updateGameConstantsStub.called);
+        expect(response.status).toBe(HttpStatus.NO_CONTENT);
+    });
     it('updateGameConstants() should return 500 if there is an error', async () => {
         updateGameConstantsStub.throws(new Error('test'));
         const response = await request(httpServer).put('/game-constants').send(DEFAULT_GAME_CONSTANTS);
