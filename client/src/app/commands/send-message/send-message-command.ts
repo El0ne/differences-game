151acc7a495be9d6302de02f9237ed6de5d30297
Elouan Guyon <elouan.guyon@polymtl.ca>
2023-04-11 23:34:30 -0400

    added messages. receive 2 times best message notif but should be one when we stop checking for best times

--- client/src/app/commands/send-message/send-message-command.ts
+++ client/src/app/commands/send-message/send-message-command.ts
@@ -1,14 +1,18 @@
 import { Command } from '@app/commands/command';
 import { SoloViewComponent } from '@app/pages/solo-view/solo-view.component';
+import { RoomMessage } from '@common/chat-gateway-constants';
 
 export class SendMessageCommand implements Command {
     soloView: SoloViewComponent;
-    constructor(soloView: SoloViewComponent) {
+    message: RoomMessage;
+
+    constructor(soloView: SoloViewComponent, message: RoomMessage) {
         this.soloView = soloView;
+        this.message = message;
     }
 
     execute(): void {
-        this.soloView.sendMessage();
+        this.soloView.messages.push(this.message);
         console.log('send message');
         console.log(' \n');
     }
