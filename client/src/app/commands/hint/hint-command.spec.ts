1953831d508b93ecac0c49c23322005c2d659f51
Chahine <tchamitouche@gmail.com>
2023-04-17 19:08:39 -0400

    two news commands tests working

--- client/src/app/commands/hint/hint-command.spec.ts
+++ client/src/app/commands/hint/hint-command.spec.ts
@@ -1,7 +1,50 @@
-// import { HintCommand } from './hint-command';
+import { HintCommand } from '@app/commands/hint/hint-command';
+import { SoloViewComponent } from '@app/pages/solo-view/solo-view.component';
 
-// describe('HintCommand', () => {
-//   it('should create an instance', () => {
-//     expect(new HintCommand()).toBeTruthy();
-//   });
-// });
+describe('HintCommand', () => {
+    let command: HintCommand;
+    let soloView: SoloViewComponent;
+
+    beforeEach(() => {
+        soloView = jasmine.createSpyObj('SoloViewComponent', ['setCurrentHint', 'hintTimeouts', 'handleHint', 'activateThirdHint']);
+        soloView.left = jasmine.createSpyObj('left', ['hintPosX', 'hintPosY']);
+        soloView.right = jasmine.createSpyObj('right', ['hintPosX', 'hintPosY']);
+        soloView.gameHintService = jasmine.createSpyObj('gameHintService', ['hintsRemaining']);
+    });
+
+    it('should create an instance of HintCommand', () => {
+        command = new HintCommand(0, 0, soloView);
+        expect(command).toBeTruthy();
+    });
+
+    it('should update hint positions on left and right components', () => {
+        command = new HintCommand(10, 20, soloView);
+        command.execute();
+        expect(soloView.left.hintPosX).toBe(10);
+        expect(soloView.left.hintPosY).toBe(20);
+        expect(soloView.right.hintPosX).toBe(10);
+        expect(soloView.right.hintPosY).toBe(20);
+    });
+
+    it('should decrease hintsRemaining on gameHintService', () => {
+        soloView.gameHintService.hintsRemaining = 2;
+        command = new HintCommand(0, 0, soloView);
+        command.execute();
+        expect(soloView.gameHintService.hintsRemaining).toBe(1);
+    });
+
+    it('should activate third hint on soloView when hintsRemaining is 0', () => {
+        soloView.gameHintService.hintsRemaining = 1;
+        command = new HintCommand(0, 0, soloView);
+        command.execute();
+        expect(soloView.activateThirdHint).toHaveBeenCalled();
+    });
+
+    it('should call setCurrentHint, hintTimeouts, and handleHint on soloView', () => {
+        command = new HintCommand(0, 0, soloView);
+        command.execute();
+        expect(soloView.setCurrentHint).toHaveBeenCalled();
+        expect(soloView.hintTimeouts).toHaveBeenCalled();
+        expect(soloView.handleHint).toHaveBeenCalled();
+    });
+});
--- client/src/app/commands/third-hint-color/third-hint-color-command.spec.ts
+++ client/src/app/commands/third-hint-color/third-hint-color-command.spec.ts
@@ -1,7 +1,21 @@
-// import { ThirdHintColorCommand } from './third-hint-color-command';
+import { SoloViewComponent } from '@app/pages/solo-view/solo-view.component';
+import { ThirdHintColorCommand } from './third-hint-color-command';
 
-// describe('ThirdHintColorCommand', () => {
-//   it('should create an instance', () => {
-//     expect(new ThirdHintColorCommand()).toBeTruthy();
-//   });
-// });
+describe('ThirdHintColorCommand', () => {
+    let soloView: SoloViewComponent;
+    let command: ThirdHintColorCommand;
+
+    beforeEach(() => {
+        soloView = jasmine.createSpyObj('SoloViewComponent', ['']);
+        command = new ThirdHintColorCommand(soloView, 'blue');
+    });
+
+    it('should create an instance', () => {
+        expect(command).toBeTruthy();
+    });
+
+    it('should execute the command', () => {
+        command.execute();
+        expect(soloView.hintColor).toEqual('blue');
+    });
+});
