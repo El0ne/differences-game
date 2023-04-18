47155f524c1a8efbbd12ef79a0fa35fa77a08bdf
Alexis Nicolas <alexisducanada@hotmail.com>
2023-04-18 19:41:08 -0400

    changed gae-constants location to assets so it can be accessed in the deploy

--- server/app/services/game-constant/game-constant.service.ts
+++ server/app/services/game-constant/game-constant.service.ts
@@ -1,11 +1,10 @@
 import { GameConstants } from '@common/game-constants';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import * as path from 'path';
 
 @Injectable()
 export class GameConstantService {
-    jsonPath = path.join(process.cwd(), 'app/dataBase/game-constants.json');
+    readonly jsonPath = 'assets/dataBase/game-constants.json';
 
     getGameConstants(): GameConstants {
         const content = fs.readFileSync(this.jsonPath, 'utf8');
