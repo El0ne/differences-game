1133d1ed12ef8751112022b15935b388298c1fa0
Chahine <tchamitouche@gmail.com>
2023-04-20 13:55:55 -0400

    100% coverage for hint command

--- client/src/app/commands/hint/hint-command.spec.ts
+++ client/src/app/commands/hint/hint-command.spec.ts
@@ -1,54 +1,37 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
-import { HintCommand } from '@app/commands/hint/hint-command';
 import { SoloViewComponent } from '@app/pages/solo-view/solo-view.component';
-import { GameHintService } from '@app/services/game-hint/game-hint.service';
+import { HintCommand } from './hint-command';
 
 describe('HintCommand', () => {
     let command: HintCommand;
     let soloView: SoloViewComponent;
-    let gameHintServiceSpy: jasmine.SpyObj<GameHintService>;
 
     beforeEach(() => {
-        soloView = jasmine.createSpyObj('SoloViewComponent', [
+        soloView = jasmine.createSpyObj<SoloViewComponent>('SoloViewComponent', [
+            'decreaseHintsRemaining',
+            'activateThirdHint',
             'setCurrentHint',
             'hintTimeouts',
             'handleHint',
-            'activateThirdHint',
-            'decreaseHintsRemaining',
-            'hintsRemaining',
         ]);
-
         soloView.left = jasmine.createSpyObj('left', ['hintPosX', 'hintPosY']);
         soloView.right = jasmine.createSpyObj('right', ['hintPosX', 'hintPosY']);
-        gameHintServiceSpy = jasmine.createSpyObj('GameHintService', ['hintsRemaining']);
-        Object.defineProperty(gameHintServiceSpy, 'hintsRemaining', { value: 3 });
+        command = new HintCommand(1, 2, soloView);
     });
 
-    it('should create an instance of HintCommand', () => {
-        command = new HintCommand(0, 0, soloView);
-        expect(command).toBeTruthy();
-    });
-
-    it('should update hint positions on left and right components', () => {
-        command = new HintCommand(10, 20, soloView);
+    it('should execute the hint command', () => {
         command.execute();
-        expect(soloView.left.hintPosX).toBe(10);
-        expect(soloView.left.hintPosY).toBe(20);
-        expect(soloView.right.hintPosX).toBe(10);
-        expect(soloView.right.hintPosY).toBe(20);
+
+        expect(soloView.left.hintPosX).toEqual(1);
+        expect(soloView.left.hintPosY).toEqual(2);
+        expect(soloView.right.hintPosX).toEqual(1);
+        expect(soloView.right.hintPosY).toEqual(2);
     });
 
-    it('should decrease hintsRemaining on gameHintService', () => {
-        command = new HintCommand(0, 0, soloView);
-        command.execute();
-        expect(soloView.decreaseHintsRemaining).toHaveBeenCalled();
-    });
+    it('should activate the third hint when no hints remaining', () => {
+        Object.defineProperty(soloView, 'hintsRemaining', { value: 0 });
 
-    it('should call setCurrentHint, hintTimeouts, and handleHint on soloView', () => {
-        command = new HintCommand(0, 0, soloView);
         command.execute();
-        expect(soloView.setCurrentHint).toHaveBeenCalled();
-        expect(soloView.hintTimeouts).toHaveBeenCalled();
-        expect(soloView.handleHint).toHaveBeenCalled();
+
+        expect(soloView.activateThirdHint).toHaveBeenCalled();
     });
 });
